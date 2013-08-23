094aeacaf3bccae2fdf1f5c92f110de7ecfe6304
Ivan Čentéš <ivan.centes@etnetera.cz>
2013-08-23 16:09:26 +0200

    DomManipulation controller + resizovani holderu pro mapu

---
+++ public/_main/controller/DomManipulation.js
@@ -0,0 +1,40 @@
+Ext.define('PumaMain.controller.DomManipulation', {
+	extend: 'Ext.app.Controller',
+	views: [],
+	requires: [],
+	init: function() {
+		$("#sidebar-reports-toggle").on("click", $.proxy(this._onSidebarToggleClick, this));
+		$(window).on("resize", $.proxy(this._onWindowResize, this))
+		
+		this.resizeMap();
+	},
+	
+	renderApp: function() {
+		$("body").removeClass("intro").addClass("application");
+		this.resizeMap();
+	},
+	
+	renderIntro: function() {
+		// zatim neni potreba
+	},
+	
+	resizeMap: function() {
+		var w = $(window).width();
+		var h = $(window).height() - $("#header").outerHeight(true) - $("#footer").outerHeight(true);
+		
+		if ($("body").hasClass("application")) {
+			w = $("#sidebar-reports").position().left;
+		}
+		
+		$("#app-map").css({width : w, height : h});
+	},
+	
+	_onSidebarToggleClick: function() {
+		$("#sidebar-reports").toggleClass("hidden");
+		this.resizeMap();
+	},
+	
+	_onWindowResize: function() {
+		this.resizeMap();
+	}
+});
