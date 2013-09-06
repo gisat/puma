82505ac9344851df1f9adc71e92d39f99a12f934
Ivan Čentéš <ivan.centes@etnetera.cz>
2013-09-06 16:19:13 +0200

    Colorpicker

--- public/_main/view/Tools.js
+++ public/_main/view/Tools.js
@@ -13,8 +13,9 @@ Ext.define('PumaMain.view.Tools', {
         this.items = [{
             xtype: 'panel',
             collapsed: true,
-            height: 60,
+            height: 80,
             title: 'Select color',
+            bodyCls: 'tools-colorpicker',
             items: [{
                     xtype: 'colorpicker',
                     fieldLabel: 'CP',
