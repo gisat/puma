2c24ef20d7c323cf89609b118262f1a440a94591
jveselka <jonas.veselka@gisat.cz>
2015-05-25 12:57:48 +0200

    Servírování statických souboru nechat dělat přímo Apache.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
     
 var express = require('express');
 var app = express();
+var conn = require('./common/conn');
 var staticFn = express['static'];
 
 var async = require('async');
@@ -17,12 +18,49 @@ function initServer(err) {
     //app.use(express.favicon(__dirname + '/public/images/puma-logo.png'));
     app.use('/printpublic',function(req,res,next) {
         if (req.path.search('.html')>-1 && req.path.search('index3')<0) {
-            return next(new Error('unauthorized'))
+            return next(new Error('unauthorized'));
         }
         return next(null);
-    })
-    app.use('/public/extjs-4.1.3',staticFn(__dirname + '/public/extjs-4.1.3', {maxAge: oneDay*7}));
-    app.use('/public',staticFn(__dirname + '/public'));
+    });
+	
+	/*
+	#######################################################################
+	Nastaveni apache.conf pro servirovani statickych souboru primo Apachem:
+	
+	Alias /help /var/www/puma-app/public/help/
+	
+	#### /tool/* static routing
+	RedirectMatch 301 ^/tool$ /tool/
+	RedirectMatch 301 ^/catalogue/(.*)$ /geonode/catalogue/$1
+
+	AliasMatch ^/tool/$ /var/www/puma-app/public/data-exploration.html
+	Alias /tool/css /var/www/puma-app/public/css
+	Alias /tool/ux /var/www/puma-app/public/ux
+	Alias /tool/_main /var/www/puma-app/public/_main
+	Alias /tool/_common /var/www/puma-app/public/_common
+	Alias /tool/images /var/www/puma-app/public/images
+	Alias /tool/devlib /var/www/puma-app/public/devlib
+	Alias /tool/lib /var/www/puma-app/public/lib
+	Alias /tool/gisatlib /var/www/puma-app/public/gisatlib
+	Alias /tool/extjs-4.1.3 /var/www/puma-app/public/extjs-4.1.3
+
+	ProxyPassMatch ^/tool/?$ !
+	ProxyPass /tool/css !
+	ProxyPass /tool/ux !
+	ProxyPass /tool/_main !
+	ProxyPass /tool/_common !
+	ProxyPass /tool/images !
+	ProxyPass /tool/devlib !
+	ProxyPass /tool/lib !
+	ProxyPass /tool/gisatlib !
+	ProxyPass /tool/extjs-4.1.3 !
+	
+	#### /tool* non-static routing (and some minor static, not covered by above code)
+	ProxyPass /tool http://127.0.0.1:3000
+	ProxyPassReverse /tool http://127.0.0.1:3000
+	 */
+
+	app.use('extjs-4.1.3',staticFn(__dirname + '/public/extjs-4.1.3', {maxAge: oneDay*7})); // jen pro jistotu, ale mel by to vyridit uz Apache
     app.use('/printpublic',staticFn(__dirname + '/public'));
     app.use(express.cookieParser());
     app.use(express.bodyParser());
@@ -30,17 +68,18 @@ function initServer(err) {
     require('./routes/security')(app);
     require('./routes/routes')(app);
     require('./routes/finish')(app);
+	app.use('/', staticFn(__dirname + '/public'));
     app.listen(3000);
     console.log('Listening on port 3000'); 
 }
 
 async.series([
-    function(callback) {
-        require('./common/conn').init(app,callback);
-    },
-    function(callback) {
-        loc.init(callback);
-    }],
-    initServer
+	function(callback) {
+		conn.init(app,callback);
+	},
+	function(callback) {
+		loc.init(callback);
+	}],
+	initServer
 );
 
