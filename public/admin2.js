9ab571ad9e7f710167a19b98259747abe5c0be70
jveselka <jonas.jonas.jonas@gmail.com>
2015-09-11 11:21:40 +0200

    Dependency injection

--- public/admin2.js
+++ public/admin2.js
@@ -1,7 +1,5 @@
 var Hello = React.createClass({
-    render: function() {
-        return <div>Hello, {this.props.name}</div>;
-    }
+	render: function() {
+		return <div>Hello, {this.props.name}</div>;
+	}
 });
-
-React.render(<Hello name="Jon" />, document.getElementById('content'));
