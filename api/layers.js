dcfe73fe14c4bcbfe7dcb10e73537047d52d7375
jveselka <jonas.jonas.jonas@gmail.com>
2015-09-16 17:11:46 +0200

    Passworrds and usernames changed to config value

--- api/layers.js
+++ api/layers.js
@@ -600,8 +600,8 @@ function getMetadata(params, req, res, callback) {
 function getSymbologiesFromServer(params, req, res, callback) {
     var opts = {
         symbologiesServer: function(asyncCallback) {
-            var username = 'admin';
-            var password = 'GeoNodeGeoServerNr1';
+            var username = config.geoserverUsername;
+            var password = config.geoserverPassword;
             var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
             var headers = {
                 'Content-type': 'application/json',
