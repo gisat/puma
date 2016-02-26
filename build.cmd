fc2863617c63bdbd7a628f0f1a5313a53f95d777
jveselka <jonas.jonas.jonas@gmail.com>
2016-02-26 16:31:36 +0100

    Build script terrible bug fix

--- build.cmd
+++ build.cmd
@@ -7,7 +7,7 @@ pause
 call sencha build -p public\appde.jsb3 -d public
 call jsmin <public\appde.all.js >public\appde.min.js
 call sencha build -p public\appmng.jsb3 -d public
-call jsmin <public\appde.all.js >public\appmng.min.js
+call jsmin <public\appmng.all.js >public\appmng.min.js
 echo.
 echo Now, uncomment 'Puma.patch.Main' lines
 echo.
