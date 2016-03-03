71ddcab459689c603312f058a15743de6e9282e7
jveselka <jonas.jonas.jonas@gmail.com>
2016-03-03 17:51:36 +0100

    Port console

--- server.js
+++ server.js
@@ -73,6 +73,7 @@ function initServer(err) {
 	require('./routes/routes')(app);
 	require('./routes/finish')(app);
 	app.use('/', staticFn(__dirname + '/public'));
+	console.log('Going to listen on port ' + config.localPort + '...');
 	app.listen(config.localPort);
 	console.log('Listening on port ' + config.localPort);
 }
