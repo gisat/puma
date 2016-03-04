6ce876659be91f19cbde41c67e135e7b37afb04c
jveselka <jonas.jonas.jonas@gmail.com>
2016-03-04 01:04:00 +0100

    auth.auth missing cookies handling

--- common/auth.js
+++ common/auth.js
@@ -25,10 +25,13 @@ function owner(req,res,next) {
 
 
 function auth(req, res, next) {
-
-	var sessionId = req.cookies['ssid'];
-	if (!sessionId)
+	if (!req.cookies){
 		return next();
+	}
+	var sessionId = req.cookies['ssid'];
+	if (!sessionId){
+		return next();
+	}
 	req.ssid = sessionId;
 	var userName = sessionCache[sessionId];
 	if (userName) {
