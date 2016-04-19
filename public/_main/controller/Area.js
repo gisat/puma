78719119ba90f1b24420119b774389b04052cbb0
jveselka <jonas.jonas.jonas@gmail.com>
2016-04-19 02:44:02 +0200

    fix bounds projection

--- public/_main/controller/Area.js
+++ public/_main/controller/Area.js
@@ -200,7 +200,9 @@ Ext.define('PumaMain.controller.Area', {
 		var locObj = this.getLocationObj();
 		var loc = locObj.location;
 		if (locObj.bbox){
-			this.getController('Map').zoomToExtent(new OpenLayers.Bounds(locObj.bbox.split(",")));
+			var bounds = new OpenLayers.Bounds(locObj.bbox.split(","))
+			bounds = bounds.transform(new OpenLayers.Projection("EPSG:4326"),new OpenLayers.Projection("EPSG:900913"));
+			this.getController('Map').zoomToExtent(bounds);
 			return;
 		}
 		var locGid = locObj.locGid;
