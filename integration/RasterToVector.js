8d844653ef4d7e1f5a2eab36fb21da65b009279c
Jakub Balhar <jakub@balhar.net>
2016-05-06 11:49:36 +0200

    Run processing script.

--- integration/RasterToVector.js
+++ integration/RasterToVector.js
@@ -1,16 +1,30 @@
 var Promise = require('promise');
+var cp = require('child_process');
 var logger = require('../common/Logger').applicationWideLogger;
 
-var RasterToVector = function(rasterFileLocation) {
-
+var RasterToVector = function(scriptsDirectoryLocation, rasterFileLocation) {
+	this.scriptLocation = scriptsDirectoryLocation + "vectorize-guf.sh";
+	this.rasterFileLocation = rasterFileLocation;
 };
 
 /**
  * @returns {Promise} promise.
  */
 RasterToVector.prototype.process = function(){
+	logger.trace('RasterToVector#process Start processing of the raster. Location: ', this.rasterFileLocation,
+		" Script: ", this.scriptsDirectoryLocation);
+
+	var self = this;
 	return new Promise(function(resolve, reject){
-		resolve();
+		cp.execFile(self.scriptLocation, [], {}, function(err, stdout, stderr) {
+			if(err) {
+				logger.error("RasterToVector#process Error processing the vector file.", err);
+				reject(err);
+			}
+
+			console.log(stdout);
+			resolve();
+		});
 	});
 };
 
