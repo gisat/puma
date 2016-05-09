dd1454edea0127ed44f3148019b22b99dbdfeed6
Jakub Balhar <jakub@balhar.net>
2016-05-09 15:51:46 +0200

    Implemented login and loadHomePage for Geonode.

--- integration/User.js
+++ integration/User.js
@@ -1,16 +1,18 @@
-var querystring = require('querystring');
 var config = require('../config');
 var conn = require('../common/conn');
 var logger = require('../common/Logger').applicationWideLogger;
 
 var Geonode = require('./Geonode');
+var Cookies = require('../common/Cookies');
 
 var User = function(username, password){
 	this.username = username;
 	this.password = password;
+
+	this.logged = false;
 	this.ssid = null;
-	this.isLogged = false;
-	
+	this.csrf = null;
+
 	this.geonode = new Geonode(config.geonodeProtocol, config.geonodeHost, config.geonodeProtocol);
 };
 
@@ -18,49 +20,34 @@ User.prototype.login = function(){
 	logger.info("User#login Username: ", this.username, " Password: ", this.password);
 
 	var self = this;
-	conn.request(loadHomePage,null,function(err,output,homePageResponse) {
-		if (err) {
-			logger.error("User#login It wasn't possible to load Geonode HomePage. Error: ", err);
-			return;
-		}
-		if(!('set-cookie' in homePageResponse.headers)) {
+	this.geonode.loadHomePage().then(function(homePageResponse){
+		var cookies = new Cookies(homePageResponse);
+		if(!cookies.atLeastOne()) {
 			logger.error("User#login Cookies weren't set for the request to the HomePage");
-			return;
+			throw new Error("User#login Cookies weren't set for the request to the HomePage");
 		}
 
-		var cookies = [];
-		homePageResponse.headers['set-cookie'][0].split(';').forEach(function(element, index, array){
-			var pair = element.split("=");
-			var key = decodeURIComponent(pair.shift()).trim();
-			var value = decodeURIComponent(pair.join("=")).trim();
-			cookies[key] = value;
-		});
-		var csrf = cookies['csrftoken'];
-		var formDataForLogin = querystring.stringify({
-			username: self.username,
-			password: self.password,
-			csrfmiddlewaretoken: csrf,
-			next: ''
-		});
-		var options2 = {
-			protocol: config.geonodeProtocol,
-			host: config.geonodeHost,
-			path: isLogin ? config.geonodePath+'/account/login/' : config.geonodePath+'/account/logout/',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				'Content-Length': postData.length,
-				'Cookie':'csrftoken='+csrf,
-				'referer': config.remoteProtocol + '://' + config.geonodeHost + '/'
-			}
-		};
-		conn.request(options2,postData,function(err,output,res2) {
-			if (err){
-				console.log("\n\nconn.geonodeCom ERROR:\nerr code:", err, "\noutput:", output);
-				return generalCallback(err);
-			}
-			return specificCallback(res2);
-		});
+		if(cookies.get("csrf") == null) {
+			logger.error("User#login Csrf wasn't present in response");
+			throw new Error("User#login Csrf wasn't present in response");
+		}
+
+		return self.geonode.login(self.username, self.password, csrf);
+	}).then(function(loginResponse){
+		// TODO validate meaningfully response.
+		var cookies = new Cookies(loginResponse);
+		self.logged = true;
+		self.ssid = cookies.get("ssid");
+		if(!self.ssid) {
+			logger.error("User#login The response doesn't contain the ssid. Probably invalid login " +
+				"information.");
+		}
+		self.csrf = cookies.get("csrf");
+		if(!self.csrf) {
+			logger.error("User#login The response doesn't contain the csrf");
+		}
+	}).catch(function(err){
+		logger.error("User#login It wasn't possible to log user into the solution. Error: ", err);
 	});
 };
 
