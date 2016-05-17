11e7f7497b3bf98ff1d311c54f9e813268dc8f35
Jakub Balhar <jakub@balhar.net>
2016-05-17 22:56:05 +0200

    Allow dataview for anyone.

--- routes/security.js
+++ routes/security.js
@@ -1,8 +1,9 @@
 var auth = require('../common/auth');
 
 module.exports = function(app) {
- 
 
+
+	app.post('/rest/dataview', auth.anyone);
 	app.post('/rest/*',auth.auth,auth.anyUser);
 	app.put('/rest/*',auth.auth,auth.anyUser);
 	app.delete('/rest/*',auth.auth,auth.anyUser);
