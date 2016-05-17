eec1186e65d4971156194afc7b9074e2b35b337a
Jakub Balhar <jakub@balhar.net>
2016-05-17 18:05:20 +0200

    Return processing to the chain.

--- routes/integration.js
+++ routes/integration.js
@@ -54,9 +54,8 @@ module.exports = function (app) {
 			processes.store(process);
 			// or like this:
 
-			return "italy";
-			/*return new RasterToPSQL(conn.getPgDataDb(), remoteFile.getDestination())
-				.process();*/
+			return new RasterToPSQL(conn.getPgDataDb(), remoteFile.getDestination())
+				.process();
 		}).then(function(rasterTableName){
 			// Run analysis // Async
 			var rasterLayerTableName = "public." + rasterTableName;
