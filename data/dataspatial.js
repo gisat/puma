d48a51ebd854f62af64926fe3c3fb2a8b7d4b166
jveselka <jonas.jonas.jonas@gmail.com>
2016-05-19 20:25:30 +0200

    Fix the :bug: getting PG connection string instead of getting PG client

--- data/dataspatial.js
+++ data/dataspatial.js
@@ -78,7 +78,7 @@ function getData(params, callback) {
 				}
 				var unitLayerName = areaId != -1 ? (unitLayerRef.layer.split(':')[1]) : ('up.base_user_' + params['userId'] + '_loc_' + locationId);
 				var unitLayerGid = areaId != -1 ? ('"'+unitLayerRef.fidColumn+'"') : 'gid';
-				var processClient = new pg.Client(conn.getPgConnString());
+				var processClient = conn.getPgDataDb();
 				processClient.connect();
 				var aggregateLayerRef = results.aggregateLayer.layerRef;
 				var sql = 'SELECT COUNT(DISTINCT a."'+aggregateLayerRef.fidColumn+'") as cnt';
