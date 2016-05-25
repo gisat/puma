4466e1ad46e5434fc08f87c9ff7438bd57b509fa
Jakub Balhar <jakub@balhar.net>
2016-05-25 09:04:43 +0200

    Add Size of Pixel based on the information in the raster layer.

---
+++ analysis/spatial/SizeOfPixel.js
@@ -0,0 +1,30 @@
+var Promise = require('promise');
+var util = require('util');
+
+var logger = require('../common/Logger').applicationWideLogger;
+var connection = require('../../common/conn');
+
+var SizeOfPixel = function(rasterLayerTable) {
+	this.rasterLayerTable = rasterLayerTable;
+};
+
+SizeOfPixel.prototype.get = function() {
+	var sql = util.format("select 100000 * st_pixelheight(rast) as x, 100000 * st_pixelwidth(rast) as y from %s;", this.rasterLayerTable);
+
+	return new Promise(function(resolve, reject){
+		connection.getPgDataDb().query(sql, function(error, results) {
+			if (error) {
+				throw new Error(
+					logger.error('SizeOfPixel#get Error with SQL: ', sql, " Error: ", error)
+				);
+			}
+
+			resolve({
+				x: results.rows[0].x,
+				y: results.rows[0].y
+			})
+		})
+	})
+};
+
+module.exports = SizeOfPixel;
