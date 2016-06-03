1efef5cb540d56aefc1d3695ea81374bae5f6500
Jakub Balhar <jakub@balhar.net>
2016-06-03 11:24:59 +0200

    Urbis config

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ module.exports = {
 	remoteProtocol  : "http",
 	remoteAddress   : "localhost:4000",
 
-	pgDataConnString   : "postgres://postgres:postgres@185.8.164.70:5432/geonode_data",
-	pgGeonodeConnString: "postgres://postgres:postgres@185.8.164.70:5432/geonode",
-	mongoConnString    : "mongodb://185.8.164.70:27017/panther",
+	pgDataConnString   : "postgres://postgres:postgres@185.8.165.230:5432/geonode_data",
+	pgGeonodeConnString: "postgres://postgres:postgres@185.8.165.230:5432/geonode",
+	mongoConnString    : "mongodb://185.8.165.230:27017/test",
 
 	workspaceSchemaMap: {
 		geonode: "public"
@@ -25,13 +25,13 @@ module.exports = {
 	//	}
 	//},
 
-	geoserverHost   : "185.8.164.70",
+	geoserverHost   : "185.8.165.230",
 	geoserverPort   : 8080,
 	geoserverPath   : "/geoserver",
 	geoserverUsername : "admin",
 	geoserverPassword : "GeoNodeGeoServerNr1",
 
-	geoserver2Host  : "185.8.164.70",
+	geoserver2Host  : "185.8.165.230",
 	geoserver2Port  : 8080,
 	geoserver2Path  : "/geoserver_i2",
 	geoserver2Username  : "admin",
@@ -39,7 +39,7 @@ module.exports = {
 	geoserver2Workspace : "puma",
 
 	geonodeProtocol : "http",
-	geonodeHost     : "185.8.164.70",
+	geonodeHost     : "185.8.165.230",
 	geonodePath     : "",
 	geonodeHome     : "/geonode",
 
