9a9397e4024b67797ff8885ee775105ae48fddae
Martin Babič <martin.babic@gisat.cz>
2016-07-08 18:28:41 +0200

    Attempt to understand how SPARQL works :)

--- public/ipr/js/query.js
+++ public/ipr/js/query.js
@@ -1,44 +1,46 @@
-$(document).ready( function () {
+$(document).ready(function () {
 
-    $("#iprQuerySearchInput").on( 'input', function () {
+    var searchField = $("#iprQuerySearchInput");
+    var searchButton = $("#iprQuerySearchButton");
+    var searchOutput = $("#iprQuerySearchOutput");
 
-        var searchOutput = $("#iprQuerySearchOutput");
-        var searchValue = $("#iprQuerySearchInput").val();
-        
-        if( searchValue.length  > 0 ) {
+    searchButton.on('click', function () {
+        executeSearch();
+    });
 
-            searchOutput.show();
+    searchField.on('keydown', function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '13') {
+            executeSearch();
+        }
+    });
 
+    function executeSearch() {
+        var searchValue = searchField.val();
+        if (searchValue.length > 0 && searchButton.val().contains("Vyhledat")) {
+            searchButton.val("Vyhledávám...");
+            searchOutput.hide();
             var searchRequest = $.ajax({
                 url: "/iprquery",
                 method: "POST",
-                data: { search: searchValue },
+                data: {search: searchValue},
                 dataType: "html"
             });
-
-            searchRequest.always(function( data, statusText, jqXHR ) {
-
-                searchOutput.show();
-
-                if ( statusText == "success" ) {
-
-                    var response = $.parseJSON( data );
-                    searchOutput.text( response.search );
-
+            searchRequest.always(function (data, statusText, jqXHR) {
+                if (statusText == "success") {
+                    var response = $.parseJSON(data);
+                    searchOutput.text(response.content);
+                    searchOutput.show();
                 } else {
-
-                    var error = $.parseJSON( data.responseText );
-                    searchOutput.text( error.message );
-
+                    var error = $.parseJSON(data.responseText);
+                    searchOutput.text(error.message);
+                    searchOutput.show();
                 }
-
+                searchButton.val("Vyhledat...");
             });
-
         } else {
-
             searchOutput.hide();
-
+            searchOutput.val("");
         }
-
-    })
+    }
 });
--- routes/iprquery.js
+++ routes/iprquery.js
@@ -1,40 +1,60 @@
 var config = require('../config.js');
 var logger = require('../common/Logger').applicationWideLogger;
-var Promise = require('promise');
-var http = require('http');
+var request = require('request');
 
 module.exports = function (app) {
 
-    app.post("/iprquery", function (request, response) {
+    app.post("/iprquery", function (req, res) {
 
-        var searchValue = request.body.search;
+        var searchValues = req.body.search.split(" ");
 
-        response.status(200).json({search: searchValue});
-
+        //response.status(200).json({search: searchValues});
         //response.status( 500 ).json( { message: "Nebyl nalezen žádný obsah!" } );
 
-        var siteDomain = "onto.fel.cvut.cz/openrdf-sesame/repositories/urban-ontology";
-        var url = "http://" + siteDomain + "?query=";
+        var url = "http://onto.fel.cvut.cz/openrdf-sesame/repositories/urban-ontology?query=";
         var query = [
             "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
             "PREFIX owl: <http://www.w3.org/2002/07/owl#>",
             "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>",
             "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
             "PREFIX common: <http://onto.fel.cvut.cz/ontologies/town-plan/common/>",
-            "PREFIX ipr: <http://onto.fel.cvut.cz/ontologies/town-plan/resource/vocab/>",
-            "SELECT DISTINCT ?dataset WHERE { ",
-            "?pojem1 common:isInContextOfDataset ?dataset . ?pojem2 common:isInContextOfDataset ?dataset FILTER (regex(str(?pojem1), \"dum\",\"i\") && regex(str(?pojem2), \"bydleni\", \"i\"))}"
-        ].join(" ");
+            "PREFIX ipr: <http://onto.fel.cvut.cz/ontologies/town-plan/resource/vocab/>"
+        ];
+        query = query.concat(["SELECT DISTINCT ?dataset WHERE {"]);
+
+        for (var valueIndex in searchValues) {
+            query = query.concat(["?pojem" + valueIndex + " common:isInContextOfDataset ?dataset"]);
+            if( valueIndex < searchValues.length - 1 ){
+                query = query.concat(["."]);
+            }
+        }
+
+        query = query.concat(["FILTER ("]);
+
+        for (var valueIndex in searchValues) {
+            query = query.concat(["regex(str(?pojem" + valueIndex + "), \".*" + searchValues[valueIndex] + ".*\", \"i\")"]);
+            if( valueIndex < searchValues.length - 1 ){
+                query = query.concat(["&&"]);
+            }
+        }
+
+        query = query.concat([")}"]);
+
+        query = query.join(" ");
+
+        console.log(query);
+
         url += encodeURIComponent(query);
-        //url += "&format=json";
-        $.ajax({
-            dataType: 'json',
-            url: url,
-            success: function ( data ) {
-                logger.error( JSON.stringify( data ) );
+
+        var content = "?";
+
+        request('http://onto.fel.cvut.cz/openrdf-sesame/repositories/urban-ontology?query=' + encodeURIComponent(query), function (iprerr, iprres, iprbody) {
+            if (!iprerr && iprres.statusCode == 200) {
+                //console.log(iprbody); // Show the HTML for the Google homepage.
+                res.status(200).send({content: iprbody});
+            } else {
+                res.status(500).send({message: iprbody});
             }
         });
-
     });
-
-}
+};
