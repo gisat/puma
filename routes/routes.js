81f5b8749460084e7ed00251653e58de4d3ab812
Jakub Balhar <jakub@balhar.net>
2016-07-12 16:59:18 +0200

    Allow creation of Theme

--- routes/routes.js
+++ routes/routes.js
@@ -151,11 +151,11 @@ module.exports = function(app) {
 
 			if(scopes.length > 1) {
 				return next(new Error('Either multiple Scopes with the same Id or multiple Scopes specified for one Theme.'));
-			} else if(scopes.length == 0) {
-				return next(new Error('There is no Scope with Id: ' + theme.dataset));
+			} else if(scopes.length == 1) {
+				// Use the years from associated Scope, if such Scope already exists. If it doesn't the years are handle by the frontend.
+				theme.years = scopes[0].years;
 			}
 
-			theme.years = scopes[0].years;
 			crud.create(req.objectType, theme, parameters, function (err, result) {
 				if (err) {
 					logger.error("It wasn't possible to create object of type: ", req.params.objType, " by User: ", req.userId,
