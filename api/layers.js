2efd0f4628ddc576e8dcb725d4d4c600b142645f
Jakub Balhar <jakub@balhar.net>
2016-07-13 13:24:58 +0200

    Add namespace to children.

--- api/layers.js
+++ api/layers.js
@@ -339,14 +339,8 @@ function parseWfsDocument(output) {
 	var wfsDocument = new xmldoc.XmlDocument(output);
 	logger.info("Wfs document " + wfsDocument);
 	var parentOfAttributes = wfsDocument.descendantWithPath("xsd:complexType.xsd:complexContent.xsd:extension.xsd:sequence");
-	logger.info("Parent of attribtues " + parentOfAttributes);
-	logger.info("Schema " + wfsDocument.descendantWithPath("xsd:complexType"));
-	logger.info("Import " + wfsDocument.descendantWithPath("xsd:complexType.xsd:complexContent"));
-	logger.info("XsdSchema " + wfsDocument.descendantWithPath("xsd:complexType.xsd:complexContent.xsd:extension"));
-	logger.info("XsdImport " + wfsDocument.descendantWithPath("xsd:complexType.xsd:complexContent.xsd:extension.xsd:sequence"));
 
-
-	var attributes = parentOfAttributes.childrenNamed("element");
+	var attributes = parentOfAttributes.childrenNamed("xsd:element");
 	var parsedAttributes = [];
 
 	attributes.forEach(function(attribute){
