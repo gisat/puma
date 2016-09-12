0c6c208dab9ddc3622ce30607e7295625abbb03a
Jakub Balhar <jakub@balhar.net>
2016-09-12 15:23:23 +0200

    Revert: Fix no bounding box Fix places in Scope. Allow only those places which are in associated scope.

--- routes/LocationController.js
+++ routes/LocationController.js
@@ -1,19 +1,9 @@
 var Controller = require('./Controller');
 
-class LocationController extends Controller {
-	constructor(app) {
-		super(app, 'location')
-	}
+var LocationController = function(app) {
+	Controller.call(this, app, 'location');
+};
 
-	create(request, response, next) {
-		var create = super.create.bind(this);
-		create(request, response, next);
-	}
-
-	update(request, response, next) {
-		var update = super.update.bind(this);
-		update(request, response, next);
-	}
-}
+LocationController.prototype = Object.create(Controller.prototype);
 
 module.exports = LocationController;
