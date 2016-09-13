49b266b66ea0ff9b10c38a032b06a4e498143058
Jakub Balhar <jakub@balhar.net>
2016-09-13 10:26:49 +0200

    Propagate to the GWC cache.

--- layers/GeoServerLayerStyles.js
+++ layers/GeoServerLayerStyles.js
@@ -4,7 +4,7 @@ var superagent = require('superagent');
  * It stores the information about the combination of layer and style.
  */
 class GeoServerLayerStyles {
-	constructor(url, userName, password){
+	constructor(url, userName, password) {
 		this._url = url;
 		this._userName = userName;
 		this._password = password;
@@ -33,6 +33,39 @@ class GeoServerLayerStyles {
 			.send({
 				style: {
 					name: style
+				}
+			}).then(this.updateGwcCache.bind(this, layerName))
+	}
+
+	/**
+	 * It allows generation of all the styles in the GWC cache.
+	 * @param layerName {String} Name of the layer as perceived by geoserver and GWC.
+	 */
+	updateGwcCache(layerName) {
+		return superagent
+			.post(this._url + '/rest/layers/' + layerName + '/styles')
+			.auth(this._userName, this._password)
+			.set('Accept', '*/*')
+			.set('Content-Type', 'application/json; charset=utf-8')
+			.send({
+				"GeoServerLayer": {
+					"enabled": true,
+					"metaWidthHeight": [4, 4],
+					"name": layerName,
+					"expireCache": 0,
+					"expireClients": 0,
+					"gridSubsets": [
+						{"gridSetName": "EPSG:900913"},
+						{"gridSetName": "EPSG:4326"}
+					],
+					"mimeFormats": [
+						"image/jpeg",
+						"image/png",
+						"image/png8",
+						"image/gif"
+					],
+					"parameterFilters": [{"defaultValue": "", "key": "STYLES"}],
+					"gutter": 0
 				}
 			})
 	}
