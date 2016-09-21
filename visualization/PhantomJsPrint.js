9b2f903f7c142f224a89f3ad96f8a00abb54989e
Jakub Balhar <jakub@balhar.net>
2016-09-21 16:53:23 +0200

    Update printing

--- visualization/PhantomJsPrint.js
+++ visualization/PhantomJsPrint.js
@@ -1,12 +1,15 @@
 var page = require('webpage').create(),
 	system = require('system');
 
+var requested = 0;
 // Use the requests and received resources to track whether everything was already downloaded.
 page.onResourceRequested = function(request) {
+	requested++;
 	console.log('Request ' + JSON.stringify(request, undefined, 4));
 };
 var lastResourceReceived = Date.now();
 page.onResourceReceived = function(response) {
+	requested--;
 	lastResourceReceived = Date.now();
 	console.log('Receive ' + JSON.stringify(response, undefined, 4));
 };
@@ -15,12 +18,15 @@ var address = system.args[1];
 var finalLocation = system.args[2];
 page.viewportSize = { width: 1600, height: 1200 };
 page.open(address, function(status) {
-	setTimeout(function(){
-		console.log(lastResourceReceived);
-		if(Date.now() - 10000 > lastResourceReceived) {
-			console.log("Everything was loaded.");
-		}
+	console.log("Status", status);
+	setTimeout(verifyAllLoaded, 2000);
+});
+
+function verifyAllLoaded() {
+	if(requested!=0) {
+		setTimeout(verifyAllLoaded, 1000);
+	} else {
 		page.render(finalLocation);
 		phantom.exit();
-	}, 60000);
-});
+	}
+}
