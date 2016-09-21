dcca3cf306ffc7e4ecb2c2c9376e948331e04935
vlach1989 <vlach1989@gmail.com>
2016-09-21 14:38:38 +0200

    pgAttributes removed

--- routes/AttributeController.js
+++ routes/AttributeController.js
@@ -1,16 +1,13 @@
 var crud = require('../rest/crud');
 var logger = require('../common/Logger').applicationWideLogger;
 var conn = require('../common/conn');
-
 var Controller = require('./Controller');
-var PgAttributes = require('../attributes/PgAttributes');
 var MongoAttributes = require('../attributes/MongoAttributes');
 var MongoAttribute = require('../attributes/MongoAttribute');
 
 class AttributeController extends Controller {
 	constructor(app, pgPool, schema) {
 		super(app, 'attribute', MongoAttributes, MongoAttribute);
-		this._pgAttributes = new PgAttributes(pgPool, schema);
 	}
 
 	read(request, response, next) {
@@ -21,26 +18,25 @@ class AttributeController extends Controller {
 		var params = {
 			attr: request.params.id,
 			attrSet: request.query.attrSet,
-			layer: request.query.layer
 		};
-	var params = {
-		attr: request.params.id,
-		attrSet: request.query.attrSet
-	};
 
 		var filter = {_id: parseInt(request.params.id)};
 
-		crud.read(this.type, filter, {userId: request.userId, justMine: request.query['justMine']}, function (err, result) {
+		crud.read(this.type, filter, {
+			userId: request.userId,
+			justMine: request.query['justMine']
+		}, function (err, result) {
 			if (err) {
 				logger.error("It wasn't possible to read item: ", request.params.objId, " from collection:", self.type, " by User:", request.userId, " Error: ", err);
 				return next(err);
 			}
 
-		result[0].attrSet = Number(params.attrSet);
-		response.data = result;
-		next();
+			result[0].attrSet = Number(params.attrSet);
+			response.data = result;
+			next();
 
-	});
-};
+		});
+	};
+}
 
 module.exports = AttributeController;
