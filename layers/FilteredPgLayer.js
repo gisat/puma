297aeb958f637f73e1680769ef7f7eebb2649e3e
Jakub Balhar <jakub@balhar.net>
2016-09-26 15:07:04 +0200

    Update parameters

--- layers/FilteredPgLayer.js
+++ layers/FilteredPgLayer.js
@@ -21,7 +21,7 @@ class FilteredPgLayer {
             path = pathReturned;
             return self.sql();
         }).then(function(sql){
-            return child_process.execFile('pgsql2shp', ["-f " + path, " -h " + config.pgDataHost, ' -u ' + config.pgDataUser, ' -P ' + config.pgDataPassword, config.pgDataDatabase, '"' + sql + '"']).promise;
+            return child_process.execFile('pgsql2shp', ["-f " + path, "-h " + config.pgDataHost, '-u ' + config.pgDataUser, '-P ' + config.pgDataPassword, config.pgDataDatabase, '"' + sql + '"']).promise;
         }).then(function(){
             return child_process.execFile('zip', [path, path + ".*"]);
         }).then(function(){
