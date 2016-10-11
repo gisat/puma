f9cac397438f04e0040c87c775ec805d85e80901
vlach1989 <vlach1989@gmail.com>
2016-10-11 12:34:32 +0200

    melodies wps pg pool added

--- melodies/WpsController.js
+++ melodies/WpsController.js
@@ -4,21 +4,21 @@ var WpsT2Process = require('./WpsT2Process');
 class WpsController {
     constructor(app, pgPool) {
         this._running = [{
-            name: 'Prague',
+            name: 'Praha',
             from: 2016,
             to: 2017,
             started: '23/9/2015',
             finished: '22/10/2015',
             status: 'Success'
         },{
-            name: 'Rome',
+            name: 'české Budějovice',
             from: 2015,
             to: 2016,
             started: '20/8/2015',
             finished: '21/8/2015',
             status: 'Failed'
         },{
-            name: 'London',
+            name: 'Brno',
             from: 2014,
             to: 2016,
             started: '20/8/2015',
