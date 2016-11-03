012ded9099954dd5de9928a4510584735f42cd09
Igor Kratochvíl <igor.kratochvil@gisat.cz>
2016-11-03 21:20:46 +0100

    sessionCache now keeps for every session complete userInfo instead of plain username.

--- common/auth.js
+++ common/auth.js
@@ -32,84 +32,23 @@ function owner(req,res,next) {
 }
 
 
+/**
+ * It does nothing else then fills the req with session variables if they are known.
+ */
 function auth(req, res, next) {
-	// So the user will logIn after that there should be sessionId and we will have information about whether the user
-	// logged succesfully
-	var sessionId = req.sessionID || req.cookies.ssid || req.ssid;
-	if (!sessionId){
-		return next();
+	var ssid = req.cookies["ssid"];
+	if (ssid){
+		var userInfo = sessionCache[ssid];
+		if (userInfo) {
+			req.userId = userInfo.userId;
+			req.groups = userInfo.groups;
+			req.isAdmin = userInfo.isAdmin;
+			req.userName = userInfo.userName;
+		}
 	}
-	req.ssid = sessionId;
-	var userName = sessionCache[sessionId];
-	if (userName) {
-		return fetchUserInfo(userName, req, sessionId, next);
-	}
-
-	var headers = {
-		'Cookie': 'sessionid=' + sessionId
-	};
-
-	// It is possible to retrieve this information from the database and actually use it for other stuff as well. 
-	var options = {
-		protocol: config.geonodeProtocol,
-		host: config.geonodeHost,
-		port: config.geonodePort || 80,
-		path: config.geonodePath + '/layers/acls',
-		headers: headers,
-		method: 'GET'
-	};
-	conn.request(options, null, function(err, output) {
-		if (err){
-			return next(err);
-		}
-		try{
-			var obj = JSON.parse(output);
-		}catch(e){
-			if(typeof obj !== 'object'){
-				throw "Failed to parse JSON <geonode>/layers/acls. Output: "+output;
-			}
-		}
-		if (!obj.name) {
-			return next();
-		}
-		return fetchUserInfo(obj.name, req, sessionId, next);
-
-	});
-
+	return next();
 }
 
-var fetchUserInfo = function(userName, req, sessionId, next) {
-
-	var client = conn.getPgGeonodeDb();
-	var sql = 'SELECT p.id, p.username, g.name ';
-		sql += 'FROM people_profile p ';
-		sql += 'LEFT JOIN people_profile_groups pg ON pg.profile_id = p.id ';
-		sql += 'LEFT JOIN auth_group g ON pg.group_id = g.id ';
-		sql += 'WHERE p.username = $1';
-
-	client.query(sql, [userName], function(err, result) {
-		if (err) {
-			logger.error("\nError on PSQL users query.\nQuery:",sql,"\nusernames:[",userName,"]\nerr:",err,"result:",result,"\n");
-			return next(err);
-		}
-		var groups = [];
-		var id = null;
-		for (var i = 0; i < result.rows.length; i++) {
-			var row = result.rows[i];
-			if (row.name) {
-				groups.push(row.name);
-			}
-			id = id || row.id;
-		}
-		req.userId = id;
-		req.groups = groups;
-		req.isAdmin = groups.indexOf('admingroup') != -1;
-		req.userName = userName;
-		sessionCache[sessionId] = userName;
-		return next();
-	});
-
-};
 
 module.exports = {
 	auth: auth,
--- routes/LoginController.js
+++ routes/LoginController.js
@@ -2,6 +2,7 @@ var superagent = require("superagent");
 var Url = require("url").Url;
 
 var config = require("../config");
+var conn = require('../common/conn');
 var logger = require('../common/Logger').applicationWideLogger;
 var sessionCache = require("../common/auth").sessionCache;
 
@@ -70,7 +71,7 @@ function geonodeLogin(username, password, geonodeHomeUrl) {
 			// Extract cookies together with csrftoken.
 			cookies = parseSetCookieHeaders(res.headers["set-cookie"]);
 			if (!cookies.hasOwnProperty("csrftoken")) {
-				throw new Error("Missing csrftoken cookie in response to /layers.")
+				throw new Error("Missing csrftoken cookie in geonode response to /layers.")
 			}
 
 			// Send login request.
@@ -91,7 +92,7 @@ function geonodeLogin(username, password, geonodeHomeUrl) {
 					if (res.status == 200) {
 						return new Unauthorized();
 					} else {
-						throw new Error(`Unknown status code {res.status} in login response.`);
+						throw new Error(logger.error(`Unhandled status code ${res.status} in geonode login response.`));
 					}
 				}).catch(function (err) {
 					// Status code 302 Found represents successful login in Geonode.
@@ -100,15 +101,14 @@ function geonodeLogin(username, password, geonodeHomeUrl) {
 						let new_cookies = parseSetCookieHeaders(err.response.headers["set-cookie"]);
 						Object.assign(cookies, new_cookies);
 						if (!cookies.hasOwnProperty("csrftoken")) {
-							throw new Error("Missing csrftoken cookie in login response.");
+							throw new Error("Missing csrftoken cookie in geonode login response.");
 						} else if (!cookies.hasOwnProperty("sessionid")) {
-							throw new Error("Missing sessionid cookie in login response.");
+							throw new Error("Missing sessionid cookie in geonode login response.");
 						} else {
 							return cookies;
 						}
 					} else {
-						logger.error(err);
-						throw new Error(`Unknown status code {err.response.status} or other error in login response.`);
+						throw new Error(logger.error(`Unhandled status code ${err.response.status} or other error in geonode login response, err=${err}.`));
 					}
 				});
 		});
@@ -121,15 +121,55 @@ function geonodeLogout(cookieHeaderLine, geonodeHomeUrl) {
 		.type('form')
 		.set('Referer', refererUrl.format())
 		.set('Cookie', cookieHeaderLine)
+		.redirects(0)
 		.then(function (res) {
-			if (res.status == 200) {
-				return true;
-			}
+			throw new Error(logger.error(`Unhandled status code ${res.status} in geonode logout response.`));
 		}).catch(function (err) {
-			logger.error(err);
-			throw new Error("Error");
+			// Status code 302 Found represents successful logout in Geonode.
+			// Status code 403 Forbidden represents expired csrf token.
+			// Other err status codes are unknown.
+			if (err.response.status == 302 || err.response.status == 403) {
+				return true;
+			} else {
+				throw new Error(logger.error(`Unhandled status code ${err.response.status} or other error in geonode logout response, err=${err}.`));
+			}
 		});
 }
+
+function geonodeFetchUserInfo (username) {
+	var client = conn.getPgGeonodeDb();
+	var sql = 'SELECT p.id AS userid, g.name AS groupname'
+		+ ' FROM people_profile p '
+		+ ' LEFT JOIN people_profile_groups pg ON pg.profile_id = p.id'
+		+ ' LEFT JOIN auth_group g ON pg.group_id = g.id '
+		+ ' WHERE p.username = $1';
+
+	return new Promise(function (resolve) {
+		client.query(sql, [username], function (err, result) {
+			if (err) {
+				throw new Error(logger.error(`Error querying user in geonode, sql=${sql}, err=${err}.`));
+			}
+			var userid = null;
+			var groups = [];
+			for (let row of result.rows) {
+				if (row.groupname) {
+					groups.push(row.groupname);
+				}
+				userid = userid || row.userid;
+			}
+			if (!userid) {
+				throw new Error(logger.error(`No such user in geonode, username=${username}.`));
+			}
+			var userInfo = {
+				userId: userid,
+				userName: username,
+				groups: groups,
+				isAdmin: groups.indexOf("admingroup") != -1
+			};
+			resolve(userInfo);
+		});
+	});
+};
 
 
 class Unauthorized {}
@@ -138,67 +178,97 @@ class Unauthorized {}
 class LoginController {
 	constructor(app) {
 		if (!app) {
-		    throw new Error(logger.error("LoginController#constructor The controller must receive valid app."));
+			throw new Error(logger.error("LoginController#constructor The controller must receive valid app."));
 		}
 		app.post("/api/login/login", this.login.bind(this));
-		app.get("/api/login/logout", this.logout.bind(this));
+		app.post("/api/login/logout", this.logout.bind(this));
 		app.post("/api/login/getLoginInfo", this.getLoginInfo.bind(this));
 	}
 
 	login(request, response, next) {
+		// Clear userInfo from session if exists.
+		var ssid = request.cookies["ssid"];
+		if (ssid) {
+			delete sessionCache[ssid];
+		};
+
+		// Log in geonode.
 		var username = request.body.username;
 		var password = request.body.password;
 		var geonodeHomeUrl = getGeonodeHomeUrl(config);
 		geonodeLogin(username, password, geonodeHomeUrl)
-			.then(function (cookies) {
-				if (cookies instanceof Unauthorized) {
-					response.status(401).end();
-				} else {
-					response.status(200);
-
-					// Set session data.
+		.then(function (cookies) {
+			if (cookies instanceof Unauthorized) {
+				response.status(401).end();
+			} else {
+				// Fetch user info from geonode.
+				return geonodeFetchUserInfo(username)
+				.then(function (userInfo) {
+					// Set userInfo into session data.
 					var ssid = cookies["sessionid"].value;
-					sessionCache[ssid] = username;
+					sessionCache[ssid] = userInfo;
 
 					// Proxy geonode cookies to user agent.
 					for (let name in cookies) {
 						response.set("Set-Cookie", cookies[name].headerLine);
 					}
 
-					// Set panther own ssid cookie.
+					// Set panther's own ssid cookie.
 					response.cookie("ssid", ssid, {httpOnly: true});
 
 					// Set JSON response data.
-					response.json({status: "ok", ssid: ssid, csrfToken: cookies["csrftoken"].value});
-				}
-			}).catch(function (err) {
-				next(err);
-			});
+					// FIXME: The complicated data structure is here due to old FrontOffice.
+					response.status(200).json({
+						data: {
+							status: "ok",
+							ssid: ssid,
+							csrfToken: cookies["csrftoken"].value
+						},
+						success: true
+					});
+				})
+			}
+		}).catch(function (err) {
+			next(err);
+		});
 	}
 
 	logout(request, response, next) {
-		geonodeLogout(request.get('Cookie'), getGeonodeHomeUrl(config))
+		var ssid = request.cookies["ssid"];
+		response.clearCookie("ssid");
+		if (ssid) {
+			delete sessionCache[ssid];
+			geonodeLogout(request.get('Cookie'), getGeonodeHomeUrl(config))
 			.then(function (result) {
-				response.clearCookie("ssid");
-				delete sessionCache[ssid];
+				// FIXME: The complicated data structure is here due to FrontOffice.
+				response.status(200).json({success: true});
 			}).catch(function (err) {
 				next(err);
 			});
+		} else {
+			// FIXME: The complicated data structure is here due to FrontOffice.
+			response.status(200).json({success: true});
+		}
 	}
 
 	getLoginInfo(request, response, next) {
-		if (request.userId) {
-			response.json(null);
+		var ssid = request.cookies["ssid"];
+		if (ssid && sessionCache[ssid]) {
+			let userInfo = sessionCache[ssid];
+			// FIXME: The complicated data structure is here due to FrontOffice.
+			response.status(200).json({
+				data: {
+					userId: userInfo.userId,
+					groups: userInfo.groups,
+					userName: userInfo.userName
+				},
+				success: true
+			});
 		} else {
-			response.json({
-				userId: request.userId,
-				groups: request.groups,
-				userName: request.userName
-			})
+			response.status(200).json(null);
 		}
 	}
 }
 
 
 module.exports = LoginController;
-
