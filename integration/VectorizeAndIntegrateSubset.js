6b29ff8f76cad4092c0d7d98eae7d704a571030f
Jakub Balhar <jakub@balhar.net>
2016-11-11 00:31:43 +0100

    Different table name for vector version.

--- integration/VectorizeAndIntegrateSubset.js
+++ integration/VectorizeAndIntegrateSubset.js
@@ -7,7 +7,7 @@ var crud = require('../rest/crud');
 class VectorizeAndIntegrateSubset {
 	constructor(tableName, locationId) {
 		this._tifLocation = tableName + '.tif';
-		this._tableName = tableName;
+		this._tableNameVector = "vec" + tableName;
 		this._locationId = locationId;
 	}
 
@@ -21,21 +21,21 @@ class VectorizeAndIntegrateSubset {
 
 	vectorize() {
 		logger.info('VectorizeAndIntegrateSubset#vectorize Start');
-		return child_process.exec(`gdal_polygonize.py /opt/utep/${this._tifLocation} -f PostgreSQL PG:"dbname='${config.pgDataDatabase}' user='${config.pgDataUser}'" ${this._tableName}`).promise;
+		return child_process.exec(`gdal_polygonize.py /opt/utep/${this._tifLocation} -f PostgreSQL PG:"dbname='${config.pgDataDatabase}' user='${config.pgDataUser}'" ${this._tableNameVector}`).promise;
 	}
 
 	importToGeonode() {
 		logger.info('VectorizeAndIntegrateSubset#importToGeonode Start');
-		return superagent.get(`http://urban-tep.gisat.cz/cgi-bin/publishlayer?l=${this._tableName}&d=datastore&p=EPSG:4326`).then(() => {
+		return superagent.get(`http://urban-tep.gisat.cz/cgi-bin/publishlayer?l=${this._tableNameVector}&d=datastore&p=EPSG:4326`).then(() => {
 			logger.info('VectorizeAndIntegrateSubset#importToGeonode published');
-			return superagent.get(`http://urban-tep.gisat.cz/cgi-bin/updatelayers?f=${this._tableName}&s=datastore&w=geonode`);
+			return superagent.get(`http://urban-tep.gisat.cz/cgi-bin/updatelayers?f=${this._tableNameVector}&s=datastore&w=geonode`);
 		})
 	}
 
 	createMetadata() {
 		logger.info('VectorizeAndIntegrateSubset#createMetadata started');
 		return crud.createPromised("layerref", {
-			"layer": `geonode:${this._tableName}`,
+			"layer": `geonode:${this._tableNameVector}`,
 			"location": this._locationId,
 			"year": 6,
 			"active": true,
