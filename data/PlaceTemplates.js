bc5ea7c74ee30030fbb02311c7a216e4baa9e219
Martin Babic <martin.babic@gisat.cz>
2016-11-11 15:59:47 +0100

    Check if place templates has data Check if place templates has data

--- data/PlaceTemplates.js
+++ data/PlaceTemplates.js
@@ -21,12 +21,33 @@ class PlaceTemplates {
         }).then(scopeId => {
             return new FilteredMongoLayerTemplate({scope: scopeId}, conn.getMongoDb()).json();
         }).then(mongoLayerTemplates => {
+
+            var promises = [];
+            var templateIds = [];
             for(var layerTemplate of mongoLayerTemplates){
+                promises.push(
+                    new FilteredMongoLayerReferences({
+                        location: this._place, areaTemplate: layerTemplate._id
+                    }, conn.getMongoDb()).json());
+                templateIds.push(layerTemplate._id);
+            }
+            return Promise.all(promises).then(values => {
+                return {
+                    templateIds: templateIds,
+                    values: values
+                }
+            });
+
+        }).then(templateSets => {
+
+            for(var index in templateSets.templateIds) {
                 data.push({
-                    _id: layerTemplate._id
-                })
+                    _id: templateSets.templateIds[index],
+                    hasData: !!templateSets.values[index].length
+                });
             }
             return { data: data, success: true };
+
         });
     }
 }
