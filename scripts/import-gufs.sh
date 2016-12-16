d7b835bf437b8f11cd4d294e2ee102812b4f5120
jveselka <jonas.jonas.jonas@gmail.com>
2016-12-16 22:35:47 +0100

    Import GUFs bash script - import sql files to DB

--- scripts/import-gufs.sh
+++ scripts/import-gufs.sh
@@ -5,6 +5,12 @@
 # configuration
 RASTER_DESTINATION="/var/lib/tomcat7/webapps/geoserver/data/data/geonode"
 SQL_TEMP_DESTINATION="/tmp/guf-sql"
+GUF_FILE_PREFIX="guf_75m_04_"
+
+DB_USER="geonode"
+DB_DATABASE="geonode_data"
+
+
 
 # raster dir doesn't exist
 if [[ ! -e ${RASTER_DESTINATION} ]]; then
@@ -26,13 +32,38 @@ fi
 # do raster2pgsql for all rasters
 cd ${RASTER_DESTINATION}
 for f in *; do
-    if [[ -d ${f} ]] && [[ ${f} == guf_75m_04_* ]]; then
+    if [[ -d ${f} ]] && [[ ${f} == ${GUF_FILE_PREFIX}* ]]; then
         NAME=${f}
         GEOTIFF="${NAME}/${NAME}.geotiff"
+        SQL_FILE="${SQL_TEMP_DESTINATION}/${NAME}.sql"
 
+        # run raster2pgsql
         if [[ -e ${GEOTIFF} ]] && [[ -f ${GEOTIFF} ]]; then
             echo ${GEOTIFF}
-            raster2pgsql -c -C -t 200x200 -F ${GEOTIFF} ${NAME} > "${SQL_TEMP_DESTINATION}/${NAME}.sql"
+            raster2pgsql -c -C -t 200x200 -F ${GEOTIFF} ${NAME} > "${SQL_FILE}"
+        fi
+
+        # file doesn't exist
+        if [[ ! -e ${SQL_FILE} ]]; then
+            echo "Error: SQL file '${SQL_FILE}' doesn't exist."
+            continue
+        fi
+
+        # file not file
+        if [[ ! -f ${SQL_FILE} ]]; then
+            echo "Error: SQL file '${SQL_FILE}' is not a file."
+            continue
+        fi
+
+        # file has no data
+        if [[ ! -s ${SQL_FILE} ]]; then
+            echo "Error: SQL file '${SQL_FILE}' has no data."
+            continue
+        fi
+
+        # run SQL files
+        if [[ -e ${SQL_FILE} ]] && [[ -f ${SQL_FILE} ]]; then
+            psql -U ${DB_USER} -d ${DB_DATABASE} -f "${SQL_FILE}"
         fi
     fi
 done
