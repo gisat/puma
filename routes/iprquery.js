ac0bc3aa668b6e22bf22d06ee38e21d1f54a58dc
vlach1989 <vlach1989@gmail.com>
2016-12-19 17:58:29 +0100

    object data enpoint

--- routes/iprquery.js
+++ routes/iprquery.js
@@ -10,7 +10,8 @@ class iprquery {
     constructor (app) {
         app.post("/iprquery/dataset", this.searching.bind(this, "dataset"));
         app.post("/iprquery/terms", this.searching.bind(this, "terms"));
-        app.post("/iprquery/data", this.iprdata.bind(this));
+        app.post("/iprquery/data", this.dataSearching.bind(this));
+        app.post("/iprquery/object", this.objectSearching.bind(this));
 
         this._datasetEndpoint = "http://onto.fel.cvut.cz:7200/repositories/ipr_datasets";
         this._prefixes = [
@@ -23,13 +24,32 @@ class iprquery {
         ];
     }
 
-    iprdata(req, res){
+    objectSearching(req, res){
+        let dataset = req.body.dataset;
+        let object = req.body.object;
+        logger.info(`INFO iprquery#objectSearching params: ` + dataset + ' ' + object);
+        var sparql = this.prepareObjectQuery(dataset, object);
+        this.endpointRequest(sparql).then(function(result){
+            res.send(result);
+        });
+    }
+
+    /**
+     * Searching in data
+     * @param req
+     * @param req.body.dataset {string} name of the dataset
+     * @param req.body.param {string} code of parameter
+     * @param req.body.value {string} value of parameter
+     * @param req.body.type {string} type of searching (data or count)
+     * @param res
+     */
+    dataSearching(req, res){
         let dataset = req.body.dataset;
         let parameter = req.body.param;
         let value = req.body.value;
         let type = req.body.type;
 
-        var sparql = this.prepareDataQuery(this._datasetEndpoint, dataset, parameter, value, type);
+        var sparql = this.prepareDataQuery(dataset, parameter, value, type);
         this.endpointRequest(sparql).then(function(result){
             result.keywords = [parameter];
             result.value = value;
@@ -59,9 +79,9 @@ class iprquery {
             logger.info(`INFO iprquery#dataset keywords: ` + keywords);
 
             if (category == "terms"){
-                sparql = this.prepareTermsQuery(this._datasetEndpoint, keywords, type);
+                sparql = this.prepareTermsQuery(keywords, type);
             } else if (category == "dataset"){
-                sparql = this.prepareDatasetQuery(this._datasetEndpoint, keywords, type);
+                sparql = this.prepareDatasetQuery(keywords, type);
             }
 
             this.endpointRequest(sparql).then(function(result){
@@ -71,8 +91,35 @@ class iprquery {
         }
     };
 
-    prepareDataQuery(url, dataset, parameter, value, type){
-        var query = url + '?query=';
+    prepareObjectQuery(dataset, object){
+        var query = this._datasetEndpoint + '?query=';
+        var prefixes = this._prefixes.join(' ');
+
+        var filter = '(?dataset = ds:' + dataset +') && regex(str(?predikat), "' + object + '", "i")';
+        var select = '?subjekt ?predikat ?objekt';
+        var limit = 'LIMIT 100';
+
+        var sparql = 'SELECT ' + select +
+            ' WHERE {?subjekt rdf:type ?dataset. ?subjekt ?predikat ?objekt.' +
+            ' FILTER (' + filter +
+            ')} ' + limit;
+
+        logger.info(`INFO iprquery#prepareDataQuery sparql: ` + sparql);
+        logger.info(`INFO iprquery#prepareDataQuery full query: ` + query + prefixes + sparql);
+
+        return query + ' ' + encodeURIComponent(prefixes + sparql);
+    }
+
+    /**
+     * Prepare SPARQL query for searching in data
+     * @param dataset
+     * @param parameter
+     * @param value
+     * @param type
+     * @returns {string}
+     */
+    prepareDataQuery(dataset, parameter, value, type){
+        var query = this._datasetEndpoint + '?query=';
         var prefixes = this._prefixes.join(' ') + ' PREFIX dataset: <http://onto.fel.cvut.cz/ontologies/town-plan/resource/vocab/' + dataset + '/>';
 
         var filter = '(?dataset = ds:' + dataset +')';
@@ -103,13 +150,12 @@ class iprquery {
 
     /**
      * Prepare sparql query for terms
-     * @param url {string} sparql endpoint url
      * @param values {Array} values for searching
      * @param type {string} operators
      * @returns {string}
      */
-    prepareTermsQuery(url, values, type){
-        var query = url + '?query=';
+    prepareTermsQuery(values, type){
+        var query = this._datasetEndpoint + '?query=';
         var prefixes = this._prefixes.join(' ');
         var sparql = ' SELECT ?pojem ?dataset ?kod WHERE {?pojem common:isInContextOfDataset ?dataset . ?pojem common:isRepresentedAsDatabaseTableAttribute ?kod.';
 
@@ -128,13 +174,12 @@ class iprquery {
 
     /**
      * Prepare sparql query for datasets
-     * @param url {string} sparql endpoint url
      * @param values {Array} values for searching
      * @param type {string} operators
      * @returns {string}
      */
-    prepareDatasetQuery(url, values, type){
-        var query = url + '?query=';
+    prepareDatasetQuery(values, type){
+        var query = this._datasetEndpoint + '?query=';
         var prefixes = this._prefixes.join(' ');
         var sparql = ' SELECT DISTINCT ?dataset WHERE {?item common:isInContextOfDataset ?dataset . ';
 
