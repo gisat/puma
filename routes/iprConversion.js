c39eebc605da865e7217be813c34a4faa6ec1a39
vlach1989 <vlach1989@gmail.com>
2016-12-21 16:18:52 +0100

    correct proj4 conversion params

--- routes/iprConversion.js
+++ routes/iprConversion.js
@@ -10,8 +10,8 @@ class iprquery {
     constructor (app) {
         app.post("/iprconversion/krovak2wgs", this.conversion.bind(this));
 
-        this._krovakEastNorth = "+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813975277778 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +units=m +no_defs";
-        this._wgs = "WGS84"
+        this._krovakEastNorth = "+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813972222222 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +towgs84=589,76,480,0,0,0,0 +units=m +no_defs";
+        this._wgs = "WGS84";
     }
 
     /**
