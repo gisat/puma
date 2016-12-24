2c6d2a8682c9a86b1bece38c5b2b9f4ef4cd7b08
Jakub Balhar <jakub@balhar.net>
2016-12-24 11:43:51 +0100

    Started with valid test for export.

--- test/functional/data/PgTableSpec.js
+++ test/functional/data/PgTableSpec.js
@@ -21,7 +21,7 @@ describe('PgTable', () => {
 
 		schema = new DatabaseSchema(pool, commonSchema);
 		schema.create().then(() => {
-			return pool.pool().query(`insert into ${commonSchema}.groups (name) VALUES ('test')`);
+			return pool.pool().query(`insert into ${commonSchema}.migration (name) VALUES ('test')`);
 		}).then(() => {
 			done();
 		}).catch(err => {
@@ -30,8 +30,12 @@ describe('PgTable', () => {
 	});
 
 	describe('#asSql', () => {
-		it('returns correct sql for the full recreation of the table', () => {
-			table.asSql();
+		it('returns correct sql for the full recreation of the table', (done) => {
+			table.asSql().then(() => {
+				done();
+			}).catch(err => {
+				done(err);
+			});
 		});
 	});
 
