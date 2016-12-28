8671de5503fae8c39c6a9bf0b3d880d9da6d8da6
Martin Babic <martin.babic@gisat.cz>
2016-12-28 18:28:15 +0100

    TemplateData endpoint correct response when no attributes or no data

--- data/TemplateData.js
+++ data/TemplateData.js
@@ -1,14 +1,14 @@
-var FilteredMongoLayerReferences = require('../layers/FilteredMongoLayerReferences');
-//var FilteredMongoLayerTemplate = require('../layers/FilteredMongoLayerTemplate');
-var FilteredMongoAttributeSets = require('../attributes/FilteredMongoAttributeSets');
-var MongoLayerTemplate = require('../layers/MongoLayerTemplate');
-//var FilteredMongoAttributes = require('../attributes/FilteredMongoAttributes');
-var PgLayer = require('../layers/PgLayer');
-//var MongoLocation = require('../metadata/MongoLocation');
-var conn = require('../common/conn');
-var Promise = require('promise');
-var _ = require('lodash');
-var superagent = require('superagent');
+let FilteredMongoLayerReferences = require('../layers/FilteredMongoLayerReferences');
+//let FilteredMongoLayerTemplate = require('../layers/FilteredMongoLayerTemplate');
+let FilteredMongoAttributeSets = require('../attributes/FilteredMongoAttributeSets');
+let MongoLayerTemplate = require('../layers/MongoLayerTemplate');
+//let FilteredMongoAttributes = require('../attributes/FilteredMongoAttributes');
+let PgLayer = require('../layers/PgLayer');
+//let MongoLocation = require('../metadata/MongoLocation');
+let conn = require('../common/conn');
+let Promise = require('promise');
+let _ = require('lodash');
+let superagent = require('superagent');
 
 class TemplateData {
     constructor(template, place, pgPool) {
@@ -20,127 +20,144 @@ class TemplateData {
     getTemplateData(pool) {
         console.log(`TemplateData #getTemplateData => template: ${this._template}, place: ${this._place}`);
 
-        var tableColumns = [];
-        var templateData = {};
+        let tableColumns = [];
+        let templateData = {};
         templateData.data = [];
         templateData.success = false;
-        var featureData = {};
+        let featureData = {};
         featureData.attributeSets = [];
 
-        var layer;
-        var fidColumn;
+        let layer;
+        let fidColumn;
 
-        var self = this;
+        let self = this;
 
-        var mongoLayerTemplate = new MongoLayerTemplate(self._template, conn.getMongoDb());
-        return mongoLayerTemplate.json().then(templateObject => {
-            var filteredMongoAttributeSets = new FilteredMongoAttributeSets({_id: {$in: templateObject.attributeSets}}, conn.getMongoDb());
-            return filteredMongoAttributeSets.json()
-        }).then(attributeSetsArray => {
-            var promises = [];
-            for (var attributeSet of attributeSetsArray) {
-                var attributes = [];
-                var attrpromises = [];
-                for (var attribute of attributeSet.attributes) {
-                    ((attribute, attributes) => {
-                        var filteredMongoLayerReferences = new FilteredMongoLayerReferences({
-                            areaTemplate: self._template,
-                            location: self._place,
-                            isData: true,
-                            attributeSet: attributeSet._id,
-                            columnMap: {
-                                $elemMatch: {
-                                    attribute: attribute
-                                }
-                            }
-                        }, conn.getMongoDb()).json().then(filteredLayerrefsArray => {
-                            //TODO: In ideal world year should be empty value
-                            var filteredLayerref = _.find(filteredLayerrefsArray, {year: 2});
-                            var columnMapAttribute;
-                            if(filteredLayerref) {
-                                layer = filteredLayerref.layer;
-                                fidColumn = filteredLayerref.fidColumn;
-
-                                columnMapAttribute = _.find(filteredLayerref.columnMap, {attribute: attribute});
-                                tableColumns.push(columnMapAttribute.column);
-                                attributes.push({
-                                    attributeKey: attribute,
-                                    value: columnMapAttribute.column
-                                });
-                            } else {
-                                var periods = [];
-                                for (var ref of filteredLayerrefsArray) {
-                                    if(ref.year != 2){
-                                        columnMapAttribute = _.find(ref.columnMap, {attribute: attribute});
-                                        tableColumns.push(columnMapAttribute.column);
-                                        periods.push({
-                                            periodKey: ref.year,
-                                            value: columnMapAttribute.column
-                                        });
-                                    }
-                                }
-                                attributes.push({
-                                    attributeKey: attribute,
-                                    periods: periods
-                                });
-                            }
-                        });
-                        attrpromises.push(filteredMongoLayerReferences);
-                    })(attribute, attributes);
-                }
-                var attsetpromise = Promise.all(attrpromises);
-                promises.push(attsetpromise);
-                ((attributeSet, attributes) => {
-                    attsetpromise.then(() => {
-                        featureData.attributeSets.push({
-                            attributeSetKey: attributeSet._id,
-                            attributes: attributes
-                        });
-                    });
-                })(attributeSet, attributes);
-            }
-            return Promise.all(promises);
-        }).then(() => {
-            tableColumns.push(fidColumn);
-            tableColumns.push('lat');
-            tableColumns.push('lon');
-            if(!layer || !fidColumn){
-                templateData.success = false;
-                templateData.message = `TemplateData #getTemplateData => Undefined => layer: ${layer}, fidColumn: ${fidColumn}`;
-                return templateData;
-            }
-            var pgLayer = new PgLayer(layer, fidColumn, null, self._pgPool);
-            return pgLayer.tableData(tableColumns).then(result => {
-                for (var row of result.rows) {
-                    templateData.data.push({
-                        key: row[fidColumn],
-                        geometry: {
-                            latitude: row.lat,
-                            longitude: row.lon
-                        },
-                        attributeSets: self.updateCollection(featureData.attributeSets, row)
-                    });
-                    // TODO Has to be removed!!!!
-                    // if(row[fidColumn] >= 100){
-                    //     break;
-                    // }
-                }
+        return new FilteredMongoLayerReferences({
+            areaTemplate: self._template,
+            location: self._place,
+            isData: false,
+            active: true
+        }, conn.getMongoDb()).json().then(baseLayerRefs => {
+            if(!baseLayerRefs.length) {
                 templateData.success = true;
                 return templateData;
+            } else if (baseLayerRefs.length != 1) {
+                return Promise.reject(`TemplateData #getTemplateData Multiple active layerrefs found`);
+            }
+
+            layer = baseLayerRefs[0].layer;
+            fidColumn = baseLayerRefs[0].fidColumn;
+
+            return new MongoLayerTemplate(self._template, conn.getMongoDb()).json().then(templateObject => {
+                let filteredMongoAttributeSets = new FilteredMongoAttributeSets({_id: {$in: templateObject.attributeSets || []}}, conn.getMongoDb());
+                return filteredMongoAttributeSets.json();
+            }).then(attributeSetsArray => {
+                let promises = [];
+                for (let attributeSet of attributeSetsArray) {
+                    let attributes = [];
+                    let attrpromises = [];
+                    for (let attribute of attributeSet.attributes) {
+                        ((attribute, attributes) => {
+                            let filteredMongoLayerReferences = new FilteredMongoLayerReferences({
+                                areaTemplate: self._template,
+                                location: self._place,
+                                isData: true,
+                                attributeSet: attributeSet._id,
+                                columnMap: {
+                                    $elemMatch: {
+                                        attribute: attribute
+                                    }
+                                }
+                            }, conn.getMongoDb()).json().then(filteredLayerrefsArray => {
+                                //TODO: In ideal world year should be empty value
+                                let filteredLayerref = _.find(filteredLayerrefsArray, {year: null});
+                                let columnMapAttribute;
+                                if (filteredLayerref) {
+                                    columnMapAttribute = _.find(filteredLayerref.columnMap, {attribute: attribute});
+                                    tableColumns.push(columnMapAttribute.column);
+                                    attributes.push({
+                                        attributeKey: attribute,
+                                        value: columnMapAttribute.column
+                                    });
+                                } else {
+                                    let periods = [];
+                                    for (let ref of filteredLayerrefsArray) {
+                                        if (ref.year) {
+                                            columnMapAttribute = _.find(ref.columnMap, {attribute: attribute});
+                                            tableColumns.push(columnMapAttribute.column);
+                                            periods.push({
+                                                periodKey: ref.year,
+                                                value: columnMapAttribute.column
+                                            });
+                                        }
+                                    }
+                                    attributes.push({
+                                        attributeKey: attribute,
+                                        periods: periods
+                                    });
+                                }
+                            });
+                            attrpromises.push(filteredMongoLayerReferences);
+                        })(attribute, attributes);
+                    }
+                    let attsetpromise = Promise.all(attrpromises);
+                    promises.push(attsetpromise);
+                    ((attributeSet, attributes) => {
+                        attsetpromise.then(() => {
+                            featureData.attributeSets.push({
+                                attributeSetKey: attributeSet._id,
+                                attributes: attributes
+                            });
+                        });
+                    })(attributeSet, attributes);
+                }
+                return Promise.all(promises);
+            }).then(() => {
+                tableColumns.push(fidColumn);
+                tableColumns.push('lat');
+                tableColumns.push('lon');
+                if (!layer || !fidColumn) {
+                    templateData.success = false;
+                    templateData.message = `TemplateData #getTemplateData => Undefined => layer: ${layer}, fidColumn: ${fidColumn}`;
+                    return templateData;
+                }
+                let pgLayer = new PgLayer(layer, fidColumn, null, self._pgPool);
+                return pgLayer.tableData(tableColumns).then(result => {
+                    for (let row of result.rows) {
+                        templateData.data.push({
+                            key: row[fidColumn],
+                            geometry: {
+                                latitude: row.lat,
+                                longitude: row.lon
+                            },
+                            attributeSets: self.updateCollection(featureData.attributeSets, row)
+                        });
+                        // TODO Has to be removed!!!!
+                        // if(row[fidColumn] >= 100){
+                        //     break;
+                        // }
+                    }
+                    templateData.success = true;
+                    return templateData;
+                });
             });
+        }).catch(error => {
+            templateData.success = false;
+            templateData.message = error;
+            return templateData;
         });
     }
 
     updateCollection(collection, row) {
-        var updatedCollection = _.cloneDeep(collection);
-        for(var attributeSet of updatedCollection) {
-            for(var attribute of attributeSet.attributes) {
-                if(attribute.hasOwnProperty('value')){
+        let updatedCollection = _.cloneDeep(collection);
+        for (let attributeSet of updatedCollection) {
+            for (let attribute of attributeSet.attributes) {
+                if (attribute.hasOwnProperty('value')) {
                     attribute.value = row[attribute.value];
                 }
-                if(attribute.hasOwnProperty('periods')){
-                    for(var period of attribute.periods){
-                        if(period.hasOwnProperty('value')){
+                if (attribute.hasOwnProperty('periods')) {
+                    for (let period of attribute.periods) {
+                        if (period.hasOwnProperty('value')) {
                             period.value = row[period.value];
                         }
                     }
