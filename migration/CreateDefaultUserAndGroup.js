f7250cc7cbe4c5bf5861e0b222ee88e32a5938df
Jakub Balhar <jakub@balhar.net>
2017-01-06 10:16:26 +0100

    Add migration which ensures that every guest has read access to current set of scopes and every user can fully modify them.

--- migration/CreateDefaultUserAndGroup.js
+++ migration/CreateDefaultUserAndGroup.js
@@ -1,4 +1,12 @@
 let Migration = require('./Migration');
+let PgPermissions = require('../security/PgPermissions');
+
+let FilteredMongoLocations = require('../metadata/FilteredMongoLocations');
+let FilteredMongoScopes = require('../metadata/FilteredMongoScopes');
+let FilteredMongoTopics = require('../metadata/FilteredMongoTopics');
+
+let Group = require('../security/Group');
+let Promise = require('promise');
 
 class CreateDefaultUserAndGroup extends Migration {
 	constructor(schema) {
@@ -14,15 +22,41 @@ class CreateDefaultUserAndGroup extends Migration {
 			return pool.pool().query(`INSERT INTO ${this.schema}.groups (name) VALUES ('guest')`);
 		}).then(() => {
 			return pool.pool().query(`INSERT INTO ${this.schema}.groups (name) VALUES ('user')`);
+		}).then(() => {
+			return this.fillDefaultPermissions();
 		});
 	}
 
-	fillDefaultPermissionForUser() {
-
+	fillDefaultPermissions(mongoDatabase, pool) {
+		let permissions = new PgPermissions(pool, this.schema);
+		new FilteredMongoScopes({}, mongoDatabase).json().then(scopes => {
+			return this.preparePermissionsForType(permissions, scopes, "dataset");
+		}).then(() => {
+			return new FilteredMongoLocations({}, mongoDatabase).json();
+		}).then(locations => {
+			return this.preparePermissionsForType(permissions, locations, "location");
+		}).then(() => {
+			return new FilteredMongoTopics({}, mongoDatabase).json();
+		}).then(topics => {
+			return this.preparePermissionsForType(permissions, topics, "topic");
+		});
+		// Load all scopes, locations, groups and topics and after the migration, guest should be able to read all and
+		// user should be able to manage all.
 	}
 
-	fillDefaultPermissionsForGuest() {
-
+	/**
+	 * It takes type such as scope and assign right to read to all guests, right to create, update and delete to users.
+	 *
+	 */
+	preparePermissionsForType(permissions, elements, type) {
+		let promises = [];
+		elements.forEach(element => {
+			promises.push(permissions.addGroup(Group.guestId(), type, element._id, "GET"));
+			promises.push(permissions.addGroup(Group.userId(), type, element._id, "PUT"));
+			promises.push(permissions.addGroup(Group.userId(), type, element._id, "DELETE"));
+			promises.push(permissions.addGroup(Group.userId(), type, null, "POST"));
+		});
+		return Promise.all(promises);
 	}
 }
 
