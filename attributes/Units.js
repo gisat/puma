4889ba7303854ffb94322cf7f3e09d3353a2289e
Jakub Balhar <jakub@balhar.net>
2017-02-08 16:45:22 +0100

    Update factor handling in the units normalization.

--- attributes/Units.js
+++ attributes/Units.js
@@ -43,9 +43,9 @@ class Units {
 		if(!unitFrom || this.allowedUnits.indexOf(unitFrom) == -1) {
 			if(this.allowedUnits.indexOf(unitTo) != -1) {
 				// Correct units give correct factor.
-				return 1 / (this.units[unitTo] * percentage);
+				return this.units[unitTo] * percentage;
 			} else {
-				return 1 / percentage;
+				return percentage;
 			}
 		}
 
