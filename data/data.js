f1ba5b9860e67af344c11572c3442892683c0172
Jakub Balhar <jakub@balhar.net>
2017-02-09 09:23:54 +0100

    Units are specified locally here.

--- data/data.js
+++ data/data.js
@@ -160,7 +160,7 @@ function getData(params, callback) {
 				factor = units.translate(attrUnits, normAttrUnits, percentage);
 				logger.info('data/data#getData Factor: ', factor, ' Attr units: ', attrUnits, ' Norm Attr Units ', normAttrUnits);
 				if(attrUnits && normAttrUnits && normalizationUnits) {
-					factor = this._units.translateFromFactorToUnits(factor, normalizationUnits);
+					factor = units.translateFromFactorToUnits(factor, normalizationUnits);
 				}
 			} else {
 				factor = percentage ? 100: 1;
