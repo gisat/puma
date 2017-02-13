e9f4a13a46f1db067cd562d1a540b0f6f04b16c6
vlach1989 <vlach1989@gmail.com>
2017-02-13 09:54:17 +0100

    convert crs only if it's not 4326

--- layers/PgAnalyticalUnits.js
+++ layers/PgAnalyticalUnits.js
@@ -7,6 +7,23 @@ class PgAnalyticalUnits {
 	}
 
 	all(analyticalUnitId) {
+		this._pool.query(`SELECT Find_SRID('views', 'layer_${analyticalUnitId}', 'the_geom');`).then(result => {
+			let srid = result.rows[0]["find_srid"];
+			// not transform
+			if (srid == 4326){
+				return this._pool.query(`SELECT gid, name, ST_AsText(the_geom) FROM views.layer_${analyticalUnitId};`).then(result => {
+					return result.rows;
+				});
+			}
+			// transform
+			else {
+				return this._pool.query(`SELECT gid, name, ST_AsText(St_Transform(the_geom, 4326)) FROM views.layer_${analyticalUnitId};`).then(result => {
+					return result.rows;
+				});
+			}
+		});
+
+
 		return this._pool.query(`SELECT gid, name, ST_AsText(St_Transform(the_geom, 4326)) FROM views.layer_${analyticalUnitId};`).then(result => {
 			return result.rows;
 		});
