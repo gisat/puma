67d216ec73fd655ce84b7fe759b20c3d0c745db7
Jakub Balhar <jakub@balhar.net>
2017-02-16 12:51:53 +0100

    Updated configuration for NewRelic.

---
+++ newrelic.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var config = require('./config');
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: [config.appName],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '0334cd6fc322cdcc1d5e90b49dc08383a664311c',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+};
