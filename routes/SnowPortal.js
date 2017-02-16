351a94e958cfafcf0f49eaadce6f40b537ed02a0
Martin Babic <martin.babic@gisat.cz>
2017-02-16 11:58:20 +0100

    SnowPortal#return scenes based on given scope

--- routes/SnowPortal.js
+++ routes/SnowPortal.js
@@ -1,4 +1,5 @@
 let _ = require("lodash");
+let Promise = require("promise");
 
 class SnowPortal {
     constructor(app, pool) {
@@ -9,19 +10,47 @@ class SnowPortal {
     }
 
     getScenesByScope(request, response) {
-
+        let scope = request.body;
+        this.getScenesSqlByScope(
+            scope
+        ).then(sql => {
+            return this._pgPool.pool().query(sql);
+        }).then(rows => {
+            if (!rows.rows || !rows.rows.length) {
+                throw new Error("Unable to get data for given scope...");
+            }
+            let queries = [];
+            _.each(rows.rows, row => {
+                queries.push(
+                    this.getScenesDataSqlBySceneAndScope(row, scope).then(sql => {
+                        return this._pgPool.pool().query(sql)
+                    })
+                );
+            });
+            return Promise.all(queries);
+        }).then(data => {
+            response.send({
+                data: data,
+                success: true
+            });
+        }).catch(error => {
+            response.send({
+                message: error.message,
+                success: false
+            });
+        });
     }
 
     getScopeOptions(request, response) {
         let options = {};
 
-        this._pgPool.pool().query(`SELECT DISTINCT "NAME" as name FROM areas`).then(rows => {
+        this._pgPool.pool().query(`SELECT DISTINCT "NAME" as name, "KEY" as key FROM areas`).then(rows => {
             if (!rows.rows) {
                 throw new Error("Unable to get areas from database...");
             }
             options.areas = _.map(rows.rows, row => {
                 return {
-                    key: row.name.toLowerCase().replace(/ /g, '-'),
+                    key: row.key,
                     name: row.name
                 }
             });
@@ -63,11 +92,123 @@ class SnowPortal {
             });
         }).catch(error => {
             response.send({
-                message: error,
+                message: error.message,
                 success: false
             });
         });
     }
+
+    getScenesSqlByScope(scope) {
+        return Promise.resolve().then(() => {
+            if (scope.area.type === "polygon") {
+                return `polygon`;
+            } else if (scope.area.type === "key") {
+                let sensors = [];
+                let satellites = [];
+                _.each(scope.sensors, (sensorSats, sensorKey) => {
+                    sensors.push(sensorKey);
+                    satellites = _.concat(satellites, sensorSats);
+                });
+
+                satellites = satellites.map(sattelite => {
+                    return `'${sattelite}'`
+                }).join(",");
+
+                sensors = sensors.map(sensor => {
+                    return `'${sensor}'`
+                }).join(",");
+
+                return (`
+                    SELECT 
+                        m.id,
+                        m.filename,
+                        s.satellite,
+                        s.sensor,
+                        m.date,
+                        100 * ST_Area(ST_Intersection(m.cxhull, a.the_geom)) / ST_Area(a.the_geom) AS perct
+                    FROM
+                        tile AS t
+                        JOIN rasters AS r ON (t.rid = r.rid)
+                        JOIN metadata AS m ON (r.metadata_id = m.id)
+                        JOIN source AS s ON (s.id = m.source_id)
+                        JOIN areas AS a ON (a."KEY" = '${scope.area.value}')
+                    WHERE
+                        s.satellite_key IN (${satellites})
+                        AND s.sensor_key IN (${sensors})
+                        AND m.date BETWEEN '${scope.timeRange.from}' AND '${scope.timeRange.to}'
+                        AND ST_Intersects(ST_SetSRID(r.extent::geometry, 3035), a.the_geom)
+                    GROUP BY
+                        m.id,
+                        m.filename,
+                        s.satellite,
+                        s.sensor,
+                        m.date,
+                        m.cxhull,
+                        a.the_geom
+                    ORDER BY
+                        m.date
+                `);
+            } else {
+                throw new Error("Unknown area type...");
+            }
+        });
+    }
+
+    getScenesDataSqlBySceneAndScope(scene, scope) {
+        return Promise.resolve().then(() => {
+            if (scope.area.type === "polygon") {
+                return `polygon`;
+            } else if (scope.area.type === "key") {
+                let sensors = [];
+                let satellites = [];
+                _.each(scope.sensors, (sensorSats, sensorKey) => {
+                    sensors.push(sensorKey);
+                    satellites = _.concat(satellites, sensorSats);
+                });
+
+                satellites = satellites.map(sattelite => {
+                    return `'${sattelite}'`
+                }).join(",");
+
+                sensors = sensors.map(sensor => {
+                    return `'${sensor}'`
+                }).join(",");
+
+                let sql = `
+                    SELECT 
+                        l.classified_as AS pixel, 
+                        Sum((pvc).count) AS count 
+                    FROM (SELECT DISTINCT 
+                            ST_ValueCount(ST_Clip(t.rast, a.the_geom)) AS pvc 
+                        FROM 
+                            metadata AS m 
+                        JOIN 
+                            rasters AS r ON (m.id=r.metadata_id) 
+                        JOIN 
+                            tile AS t ON (r.rid=t.rid)
+                        JOIN
+                            areas AS a ON (a."KEY" = '${scope.area.value}')
+                        WHERE 
+                            m.id=${scene.id}) AS foo
+                    JOIN 
+                        legend AS l ON (l.value=(pvc).value)
+                    JOIN 
+                        source AS s ON (l.source_id=s.id)
+                    JOIN 
+                        metadata AS m ON (m.source_id=s.id)
+                    WHERE 
+                        m.id=${scene.id}
+                    GROUP BY 
+                        l.classified_as;
+                `;
+                console.log(sql);
+                return sql;
+            } else {
+                throw new Error("Unknown area type...");
+            }
+        });
+    }
 }
+
 
 module.exports = SnowPortal;
