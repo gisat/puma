e90833f8c3a6bd9789286dfd4f98ce49a40ae785
jveselka <jonas.jonas.jonas@gmail.com>
2017-02-20 19:14:25 +0100

    Script for adding gufstat_km column to previously computed and update guf-to-gadm to compute only that one.

--- scripts/guf-to-gadm.sh
+++ scripts/guf-to-gadm.sh
@@ -19,52 +19,33 @@ BEGIN;
 
 -- 1 ALTER TABLE - add columns
 DO \$\$
-	BEGIN
-		BEGIN
-			ALTER TABLE "${layers[$i]}" ADD COLUMN gufstat_uf double precision;
-		EXCEPTION
-			WHEN duplicate_column THEN RAISE WARNING 'column gufstat_uf already exists.';
-		END;
-	END;
-\$\$;
-DO \$\$
-	BEGIN
-		BEGIN
-			ALTER TABLE "${layers[$i]}" ADD COLUMN gufstat_all double precision;
-		EXCEPTION
-			WHEN duplicate_column THEN RAISE WARNING 'column gufstat_all already exists.';
-		END;
-	END;
-\$\$;
-DO \$\$
-	BEGIN
-		BEGIN
-			ALTER TABLE "${layers[$i]}" ADD COLUMN gufstat_ratio double precision;
-		EXCEPTION
-			WHEN duplicate_column THEN RAISE WARNING 'column gufstat_ratio already exists.';
-		END;
-	END;
+  BEGIN
+    BEGIN
+      ALTER TABLE "${layers[$i]}" ADD COLUMN gufstat_km double precision;
+    EXCEPTION
+      WHEN duplicate_column THEN RAISE WARNING 'column gufstat_km already exists.';
+    END;
+  END;
 \$\$;
 
 -- 2 UPDATE - add data
 UPDATE "${layers[$i]}" au
-	SET
-		gufstat_uf = gufStats.v255sum,
-		gufstat_all = gufStats.v255sum + gufStats.v0sum,
-		gufstat_ratio = CASE
-			WHEN (gufStats.v255sum + gufStats.v0sum) <> 0 THEN (gufStats.v255sum::NUMERIC / (gufStats.v255sum::NUMERIC + gufStats.v0sum::NUMERIC))
-			ELSE 0
-		END
-	FROM (
-		SELECT
-			gsau.the_geom AS the_geom,
-			SUM(ST_ValueCount(ST_Clip(gsguf.rast, gsau.the_geom), 0.0)) AS v0sum,
-			SUM(ST_ValueCount(ST_Clip(gsguf.rast, gsau.the_geom), 255.0)) AS v255sum
-			FROM "${layers[$i]}" gsau
-				INNER JOIN guf_75m_04 gsguf ON ST_Intersects(gsguf.rast, gsau.the_geom)
-			GROUP BY gsau.the_geom
-	) AS gufStats
-	WHERE au.the_geom = gufStats.the_geom;
+  SET
+    gufstat_km = CASE
+      WHEN gufStats.v255sum IS NULL THEN 0
+      WHEN gufStats.v255sum::NUMERIC = 0 THEN 0
+      ELSE (ST_Area(geography(au.the_geom))/1000000) * (gufStats.v255sum::NUMERIC / (gufStats.v255sum::NUMERIC + gufStats.v0sum::NUMERIC))
+    END
+  FROM (
+    SELECT
+      gsau.the_geom AS the_geom,
+      SUM(ST_ValueCount(ST_Clip(gsguf.rast, gsau.the_geom), 0.0)) AS v0sum,
+      SUM(ST_ValueCount(ST_Clip(gsguf.rast, gsau.the_geom), 255.0)) AS v255sum
+      FROM "${layers[$i]}" gsau
+        INNER JOIN guf_75m_04 gsguf ON ST_Intersects(gsguf.rast, gsau.the_geom)
+      GROUP BY gsau.the_geom
+  ) AS gufStats
+  WHERE au.the_geom = gufStats.the_geom;
 
 COMMIT;
 EOF
