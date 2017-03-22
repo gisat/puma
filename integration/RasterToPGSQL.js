10011cb815ada7e7f6c08f419bf6783b24778870
Martin Babic <martin.babic@gisat.cz>
2017-03-22 11:08:22 +0100

    Raster2PGSql middleware class

---
+++ integration/RasterToPGSQL.js
@@ -0,0 +1,33 @@
+let fs = require('fs');
+let path = require('path');
+let Promise = require('promise');
+let child_process = require('pn/child_process');
+
+class RasterToPGSQL {
+    constructor(pgHost, pgUser, pgPassword, pgDatabase) {
+        this._pgHost = pgHost;
+        this._pgUser = pgUser;
+        this._pgPassword = pgPassword;
+        this._pgDatabase = pgDatabase;
+    }
+    
+    import(layer) {
+        return new Promise((resolve, reject) => {
+            child_process.exec(`
+                raster2pgsql -d -I -C -k -F -s 3035 -t 512x512 ${layer.path} public."${layer.name}" | PGPASSWORD=${this._pgPassword} psql -h ${this._pgHost} -U ${this._pgUser} ${this._pgDatabase}
+            `, (error, stdout, stderr) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    layer.rasterToPgsqlOutput = {
+                        stdout: stdout,
+                        stderr: stderr
+                    };
+                    resolve(layer);
+                }
+            });
+        });
+    }
+}
+
+module.exports = RasterToPGSQL;
