c19fb9b77e5c87c815ce6c649591d1a1290aa63a
Jakub Balhar <jakub@balhar.net>
2017-03-30 09:46:36 +0200

    Number against Text dichotomy.

--- layers/FrontOfficeLayers.js
+++ layers/FrontOfficeLayers.js
@@ -90,8 +90,7 @@ class FrontOfficeLayers {
 		var layers = references.map(reference => {
 			let layerTemplate = layerTemplates[reference.areaTemplate];
 			let layerGroup = layerGroups[layerTemplate.layerGroup];
-			console.log(layerTemplate.symbologies);
-			let layerStyles = styles.filter(style => layerTemplate && layerTemplate.symbologies && layerTemplate.symbologies.indexOf(style.id) !== -1 || false).map(style => {
+			let layerStyles = styles.filter(style => layerTemplate && layerTemplate.symbologies && layerTemplate.symbologies.indexOf(Number(style.id)) !== -1 || false).map(style => {
 				return {
 					name: style.name,
 					path: style.symbology_name
