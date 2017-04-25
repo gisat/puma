aacb5eaae2917c6fe4c7835c2e9e40f43bd76995
Martin Babic <martin.babic@gisat.cz>
2017-04-25 16:33:13 +0200

    WpsController#implement layer import over wps

--- integration/WpsController.js
+++ integration/WpsController.js
@@ -3,118 +3,138 @@ let conn = require('../common/conn');
 let MongoLocations = require('../metadata/MongoLocations');
 let MongoScopes = require('../metadata/MongoScopes');
 let config = require('../config');
+let _ = require('lodash');
+
+let LayerImporter = require('../integration/LayerImporter');
+let LayerImporterTasks = require('../integration/LayerImporterTasks');
 
 
-let currentProcess = {
-
-};
+let currentProcess = {};
 
 class WpsController {
-	/**
-	 *
-	 * @param app
-	 * @param pgPool
-	 * @param mongo
-	 * @param wpsProcesses {Map} Map of the WPS processes available to run
-	 */
-	constructor(app, pgPool, mongo, wpsProcesses) {
-		app.get('/rest/wps', this.wpsGet.bind(this));
-		app.post('/rest/wps', this.wpsPost.bind(this));
-		app.post('/rest/status/wps/:id', this.status.bind(this));
-		app.get('/rest/inputs/wps', this.inputs.bind(this));
-
-		this.mapToAllPlaces = new MapToAllPlaces(pgPool, mongo);
-	}
-
-	/**
-	 *
-	 * @param request
-	 * @param response
-	 */
-	wpsGet(request, response) {
+    /**
+     *
+     * @param app
+     * @param pgPool
+     * @param mongo
+     * @param wpsProcesses {Map} Map of the WPS processes available to run
+     */
+    constructor(app, pgPool, mongo, wpsProcesses) {
+        app.get('/rest/wps', this.wpsGet.bind(this));
+        app.post('/rest/wps', this.wpsPost.bind(this));
+        app.post('/rest/status/wps/:id', this.status.bind(this));
+        app.get('/rest/inputs/wps', this.inputs.bind(this));
+        
+        this.mapToAllPlaces = new MapToAllPlaces(pgPool, mongo);
+        
+        this._layerImporterTasks = new LayerImporterTasks();
+        this._layerImporter = new LayerImporter(pgPool, mongo, this._layerImporterTasks);
+    }
+    
+    /**
+     *
+     * @param request
+     * @param response
+     */
+    wpsGet(request, response) {
         let requestAction = request.query.request.toLowerCase();
-		if(requestAction === 'getcapabilities') {
-			this.getCapabilities(response);
-		} else if(requestAction === 'describeprocess') {
-			this.describeProcess(request.query.identifier, response);
-        } else if(requestAction === 'execute') {
+        if (requestAction === 'getcapabilities') {
+            this.getCapabilities(response);
+        } else if (requestAction === 'describeprocess') {
+            this.describeProcess(request.query.identifier, response);
+        } else if (requestAction === 'execute') {
             this.execute(request, response);
-		} else {
-			response.status(400).json({status: "Incorrect request. Valid choices are GetCapabilities and DescribeProcess for GET request."})
-		}
-	}
-
-	/**
-	 *
-	 * @param request
-	 * @param response
-	 */
-	wpsPost(request, response) {
-		let requestAction = request.body.request.toLowerCase();
-		if(requestAction === 'getcapabilities') {
-			this.getCapabilities(response);
-		} else if(requestAction === 'describeprocess') {
-			this.describeProcess(request.body.identifier, response);
-		} else if(requestAction === 'execute') {
-			this.execute(request, response);
-		} else {
-			response.status(400).json({status: "Incorrect request. Valid choices are GetCapabilities, DescribeProcess and Execute for POST request."})
-		}
-	}
-
-	inputs(request, response) {
-		let id = request.params.id;
-
-		if(currentProcess[id] && currentProcess[id].file) {
-			response.download(currentProcess[id].file);
-		} else {
-			response.status(400).json({status: "Incorrect request. Process with given id doesn't exist."});
-		}
-	}
-
-	status(request, response) {
-		let id = request.params.id;
-
-		if(currentProcess[id]) {
-			let data = '';
-			if(currentProcess[id].file) {
-				data = `
-					<wps:Input>
-						<ows:Identifier>data</ows:Identifier>
-						<ows:Title>Data</ows:Title>
-						<wps:Reference xlink:href="${config.remoteProtocol}/${config.remoteAddress}${config.projectHome}/inputs/wps/${id}" method="GET" mimeType="application/zip" encoding="UTF-8" />
-					</wps:Input>
-				`;
-			}
-
-			let scope = '';
-			if(currentProcess[id].scope) {
-				scope = `
-					<wps:Input>
-						<ows:Identifier>scope</ows:Identifier>
-						<ows:Title>Scope</ows:Title>
-						<wps:Data>
-							<wps:LiteralData>${currentProcess[id].scope}</wps:LiteralData>
-						</wps:Data>
-					</wps:Input>
-				`;
-			}
-
-			let place = '';
-			if(currentProcess[id].place) {
-				place = `
-					<wps:Input>
-						<ows:Identifier>place</ows:Identifier>
-						<ows:Title>Place</ows:Title>
-						<wps:Data>
-							<wps:LiteralData>${currentProcess[id].place}</wps:LiteralData>
-						</wps:Data>
-					</wps:Input>
-				`;
-			}
-
-			response.set('Content-Type', 'text/xml');
-			response.send(`
+        } else {
+            response.status(400).json({status: "Incorrect request. Valid choices are GetCapabilities and DescribeProcess for GET request."})
+        }
+    }
+    
+    /**
+     *
+     * @param request
+     * @param response
+     */
+    wpsPost(request, response) {
+        Promise.resolve().then(() => {
+            if (request.headers['content-type'] !== 'application/xml') throw new Error(`Content type 'application/xml' is expected!`);
+            return this.parseMethodInputsFromXmlDocument(request);
+        }).then(wpsInputs => {
+                if (wpsInputs.operation === 'getcapabilities') {
+                    this.getCapabilities(response);
+                } else if (wpsInputs.operation === 'describeprocess') {
+                    this.describeProcess(request.body.identifier, response);
+                } else if (wpsInputs.operation === 'execute') {
+                    this.execute(request, response, wpsInputs);
+                } else {
+                    throw new Error(`Incorrect request. Valid choices are GetCapabilities, DescribeProcess and Execute for POST request.`);
+                }
+            }
+        ).catch(error => {
+            response.status(400).json({status: `${error.message}`})
+        });
+    }
+    
+    inputs(request, response) {
+        let id = request.params.id;
+        
+        if (currentProcess[id] && currentProcess[id].file) {
+            response.download(currentProcess[id].file);
+        } else {
+            response.status(400).json({status: "Incorrect request. Process with given id doesn't exist."});
+        }
+    }
+    
+    status(request, response) {
+        let id = request.params.id;
+        
+        if (currentProcess[id]) {
+            let statusData = {}, process = ``, status = ``, created = ``, ended = ``, progress = ``, error = ``;
+            
+            if (currentProcess[id].method === `CustomLayerImport`) {
+                let layerImportTask = this._layerImporterTasks.getImporterTask(id);
+                console.log(layerImportTask);
+                statusData.method = currentProcess[id].method;
+                statusData.status = layerImportTask.status;
+                statusData.progress = layerImportTask.progress;
+                statusData.started = layerImportTask.started;
+                statusData.ended = layerImportTask.ended;
+                statusData.message = layerImportTask.message;
+            }
+            
+            if (statusData.method) {
+                process = `
+                        <wps:Process>
+					        <ows:Identifier>${statusData.method}</ows:Identifier>
+				        </wps:Process>
+                    `;
+            }
+            
+            if(statusData.status) {
+                status = `
+                        <wps:Status started="${statusData.started}" ended="${statusData.ended || ''}">
+					        <ows:Value>${statusData.status}</ows:Value>
+				        </wps:Status>
+                    `;
+            }
+            
+            if(statusData.progress >= 0) {
+                progress = `
+                        <wps:Progress>
+					        <ows:Value>${statusData.progress}</ows:Value>
+				        </wps:Progress>
+                    `;
+            }
+            
+            if(statusData.message) {
+                error = `
+                        <wps:Error>
+					        <ows:Value>${statusData.message}</ows:Value>
+				        </wps:Error>
+                    `;
+            }
+            
+            response.set('Content-Type', 'application/xml');
+            response.send(`
 				<wps:ExecuteResponse
 					xmlns:wps="http://www.opengis.net/wps/1.0.0"
 					xmlns:ows="http://www.opengis.net/ows/1.1"
@@ -124,118 +144,70 @@ class WpsController {
 					service="WPS"
 					version="1.0.0"
 					xml:lang="en-CA"
-					serviceInstance="${config.remoteProtocol}/${config.remoteAddress}${config.projectHome}/wps"
-					statusLocation="${config.remoteProtocol}/${config.remoteAddress}${config.projectHome}/status/wps/${id} ">
-				<wps:Process wps:processVersion="1">
-					<ows:Identifier>ImportToExistingScope</ows:Identifier>
-					<ows:Title>Import new data to existing Scope</ows:Title>
-					<ows:Abstract>There must be valid User in the application. It also assumes that there is at least one valid Scope in the application with associated analytical units. It is always running asynchronously.</ows:Abstract>
-				</wps:Process>
-				<wps:Status creationTime="2016-04-18T12:13:14Z">
-					<wps:ProcessSucceeded>${config.remoteProtocol}/${config.remoteAddress}${config.projectHome}/?id=2343</wps:ProcessSucceeded>
-				</wps:Status>
-				<wps:DataInputs>
-					${data}
-					${scope}
-					${place}
-				</wps:DataInputs>
-			
-				<wps:ProcessOutputs></wps:ProcessOutputs>
-			</wps:ExecuteResponse>
+					serviceInstance="${config.remoteProtocol}/${config.remoteAddress}${config.projectHome}/backend/rest/wps"
+					statusLocation="${config.remoteProtocol}/${config.remoteAddress}${config.projectHome}/backend/rest/status/wps/${id}">
+				    ${process}
+				    ${status}
+				    ${progress}
+				    ${error}
+    			</wps:ExecuteResponse>
 			`);
-		} else {
-			response.status(400).json({status: "Incorrect request. Process with given id doesn't exist."})
-		}
-	}
-
-	/**
-	 *
-	 * @param request
-	 * @param response
-	 */
-	execute(request, response) {
-		let id = conn.getNextId();
-
-		currentProcess[id] = {
-			scope: request.body.scope,
-			place: request.body.place,
-			file: request.files[0]
-		};
-
-		response.set('Content-Type', 'text/xml');
-		response.send(`
-			<wps:ExecuteResponse
-					xmlns:wps="http://www.opengis.net/wps/1.0.0"
-					xmlns:ows="http://www.opengis.net/ows/1.1"
-					xmlns:xlink="http://www.w3.org/1999/xlink"
-					xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-					xsi:schemaLocation="http://www.opengis.net/wps/1.0.0/wpsExecute_response.xsd"
-					service="WPS"
-					version="1.0.0"
-					xml:lang="en-CA"
-					serviceInstance="${config.remoteProtocol}/${config.remoteAddress}${config.projectHome}/wps"
-					statusLocation="${config.remoteProtocol}/${config.remoteAddress}${config.projectHome}/status/wps/${id} ">
-				<wps:Process wps:processVersion="1">
-					<ows:Identifier>ImportToExistingScope</ows:Identifier>
-					<ows:Title>Import new data to existing Scope</ows:Title>
-					<ows:Abstract>There must be valid User in the application. It also assumes that there is at least one valid Scope in the application with associated analytical units. It is always running asynchronously.</ows:Abstract>
-				</wps:Process>
-				<wps:Status creationTime="2016-04-18T12:13:14Z">
-					<wps:ProcessStarted/>
-				</wps:Status>
-				<wps:DataInputs>
-					<wps:Input>
-						<ows:Identifier>data</ows:Identifier>
-						<ows:Title>Data</ows:Title>
-						<wps:Reference xlink:href="${config.remoteProtocol}/${config.remoteAddress}${config.projectHome}/inputs/wps/${id}" method="GET" mimeType="application/zip" encoding="UTF-8" />
-					</wps:Input>
-					<wps:Input>
-						<ows:Identifier>scope</ows:Identifier>
-						<ows:Title>Scope</ows:Title>
-						<wps:Data>
-							<wps:LiteralData>1</wps:LiteralData>
-						</wps:Data>
-					</wps:Input>
-					<wps:Input>
-						<ows:Identifier>place</ows:Identifier>
-						<ows:Title>Place</ows:Title>
-						<wps:Data>
-							<wps:LiteralData>1</wps:LiteralData>
-						</wps:Data>
-					</wps:Input>
-				</wps:DataInputs>
-			
-				<wps:ProcessOutputs></wps:ProcessOutputs>
-			</wps:ExecuteResponse>
-		`);
-	}
-
-	/**
-	 *
-	 * @param identifier {String}
-	 * @param response
-	 */
-	describeProcess(identifier, response) {
-		let description;
-
-		if(identifier == 'ImportToExistingScope') {
-			description = this.mapToAllPlaces.describe();
-		} else {
-			response.status(400).json({status: "Err: Not supported identifier."});
-			return;
-		}
-
-		response.set('Content-Type', 'text/xml');
-		response.send(description);
-	}
-
-	/**
-	 *
-	 * @param response
-	 */
-	getCapabilities(response) {
-		response.set('Content-Type', 'text/xml');
-		response.send(`
+        } else {
+            response.status(400).json({status: "Incorrect request. Process with given id doesn't exist."})
+        }
+    }
+    
+    /**
+     *
+     * @param request
+     * @param response
+     */
+    execute(request, response, wpsInputs) {
+        switch (wpsInputs.method) {
+            case `CustomLayerImport`:
+                this.parseLayerImporterInputsFromWpsInputs(wpsInputs).then(layerImporterInputs => {
+                    this._layerImporter.importLayer(layerImporterInputs);
+                    let currentImporterTask = this._layerImporter.getCurrentImporterTask();
+                    currentProcess[currentImporterTask.id] = {
+                        created: new Date(),
+                        method: wpsInputs.method
+                    };
+                    request.params.id = currentImporterTask.id;
+                    this.status(request, response);
+                });
+                break;
+            default:
+                response.status(400).json({status: `Method ${inputs.method} was not found!`});
+                break;
+        }
+    }
+    
+    /**
+     *
+     * @param identifier {String}
+     * @param response
+     */
+    describeProcess(identifier, response) {
+        let description;
+        
+        if (identifier == 'ImportToExistingScope') {
+            description = this.mapToAllPlaces.describe();
+        } else {
+            response.status(400).json({status: "Err: Not supported identifier."});
+            return;
+        }
+        
+        response.set('Content-Type', 'text/xml');
+        response.send(description);
+    }
+    
+    /**
+     *
+     * @param response
+     */
+    getCapabilities(response) {
+        response.set('Content-Type', 'application/xml');
+        response.send(`
 			<wps:Capabilities xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:wps="http://www.opengis.net/wps/1.0.0" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xml:lang="en" service="WPS" version="1.0.0" xsi:schemaLocation="http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd">
 				<ows:ServiceIdentification>
 					<ows:Title>Prototype Panther WPS</ows:Title>
@@ -292,7 +264,43 @@ class WpsController {
 				</wps:Languages>
 			</wps:Capabilities>
 		`);
-	}
+    }
+    
+    parseLayerImporterInputsFromWpsInputs(wpsInputs) {
+        return Promise.resolve().then(() => {
+            let layerImporterInputs = {
+                user: wpsInputs.user
+            };
+            
+            _.each(wpsInputs.arguments, argument => {
+                layerImporterInputs[argument.argument] = argument.value;
+            });
+            
+            layerImporterInputs.name = layerImporterInputs.url.split(`/`).pop();
+            
+            return layerImporterInputs;
+        });
+    }
+    
+    /**
+     * Parse method inputs from XML document
+     * @param request
+     */
+    parseMethodInputsFromXmlDocument(request) {
+        return Promise.resolve().then(() => {
+            return {
+                operation: Object.keys(request.body)[0].split(`:`).pop(),
+                method: request.body[Object.keys(request.body)[0]][`ows:identifier`][0],
+                user: {
+                    id: request.session.user.id
+                },
+                arguments: _.map(request.body[Object.keys(request.body)[0]][`wps:datainputs`][0][`wps:input`], param => {
+                    return {argument: param[`ows:identifier`][0], value: param[`wps:data`][`0`][`wps:literaldata`][0]}
+                }),
+            };
+        });
+    }
 }
+
 
 module.exports = WpsController;
