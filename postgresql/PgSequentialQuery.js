635fcec356bc32cebd176e9b3b195dbc3336e391
Jakub Balhar <jakub@balhar.net>
2017-05-16 14:14:38 +0200

    Up to 12 cores.

--- postgresql/PgSequentialQuery.js
+++ postgresql/PgSequentialQuery.js
@@ -1,5 +1,7 @@
 let Promise = require('promise');
 
+let logger = require('../common/Logger').applicationWideLogger;
+
 /**
  * This class is used when there are multiple queries being called and there is at least possibly too many of them so
  * you want to prevent overloading databases.
@@ -12,8 +14,10 @@ class PgSequentialQuery {
 	// What If I ran this in groups of four, which is usually the amount of cores.
 	query(queries) {
 		if(queries.length > 20) {
+			logger.info(`AttributeController#statistics Queries: ${queries.length}`);
+
 			// Split into for groups.
-			let amountInGroup = queries.length / 6;
+			let amountInGroup = queries.length / 12;
 			let multipleResults = [];
 			return Promise.all([
 				this.handleSetOfQueries(queries.slice(0, amountInGroup)),
@@ -21,7 +25,13 @@ class PgSequentialQuery {
 				this.handleSetOfQueries(queries.slice(amountInGroup * 2, amountInGroup * 3)),
 				this.handleSetOfQueries(queries.slice(amountInGroup * 3, amountInGroup * 4)),
 				this.handleSetOfQueries(queries.slice(amountInGroup * 4, amountInGroup * 5)),
-				this.handleSetOfQueries(queries.slice(amountInGroup * 5, queries.length))
+				this.handleSetOfQueries(queries.slice(amountInGroup * 5, amountInGroup * 6)),
+				this.handleSetOfQueries(queries.slice(amountInGroup * 6, amountInGroup * 7)),
+				this.handleSetOfQueries(queries.slice(amountInGroup * 7, amountInGroup * 8)),
+				this.handleSetOfQueries(queries.slice(amountInGroup * 8, amountInGroup * 9)),
+				this.handleSetOfQueries(queries.slice(amountInGroup * 9, amountInGroup * 10)),
+				this.handleSetOfQueries(queries.slice(amountInGroup * 10, amountInGroup * 11)),
+				this.handleSetOfQueries(queries.slice(amountInGroup * 11, queries.length))
 			]).then(results => {
 				results.forEach(resultSet => {
 					Array.prototype.push.apply(multipleResults, resultSet);
