916f2cf43293fc43bcd062e503a122889a5cce2d
Martin Babic <martin.babic@gisat.cz>
2017-05-29 12:13:01 +0200

    SnowPortal#getComposites: return correct aoi value fix

--- routes/SnowPortal.js
+++ routes/SnowPortal.js
@@ -392,17 +392,16 @@ class SnowPortal {
             SELECT
               (clipped_raster_data.pvc).value                          AS class,
               sum((clipped_raster_data.pvc).count)               AS count,
-              max(100 * (clipped_raster_area.area / total_area.area)) AS aoi
+              aoi.aoi AS aoi
             FROM
               (SELECT st_valuecount(st_clip(composite.rast, g.the_geom)) AS pvc
                FROM composites."${tableName}" AS composite INNER JOIN ${geometryTable} AS g
                    ON ${geometryTableCondition} AND st_intersects(g.the_geom, composite.rast)) AS clipped_raster_data,
-              (SELECT st_area(st_union(st_polygon(st_clip(composite.rast, g.the_geom)))) AS area
-               FROM composites."${tableName}" AS composite INNER JOIN ${geometryTable} AS g
-                   ON ${geometryTableCondition} AND st_intersects(g.the_geom, composite.rast)) AS clipped_raster_area,
-              (SELECT st_area(st_union(g.the_geom)) AS area
-               FROM ${geometryTable} AS g
-               WHERE ${geometryTableCondition}) AS total_area
+              (SELECT 100 * (st_area(st_union(st_intersection(st_polygon(st_clip(r.rast, g.the_geom)), g.the_geom))) /
+                       st_area(st_union(g.the_geom))) AS aoi
+                 FROM composites."${tableName}" AS r
+                   INNER JOIN ${geometryTable} AS g ON ${geometryTableCondition}
+                 WHERE st_intersects(r.rast, g.the_geom)) AS aoi
             GROUP BY class) AS foo
           INNER JOIN source AS s ON s.satellite_key = ${this.convertArrayToSqlAny(satellites)} AND s.sensor_key = ${this.convertArrayToSqlAny(sensors)}
           INNER JOIN legend AS l ON l.source_id = s.id AND foo.class BETWEEN l.value_from AND l.value_to
