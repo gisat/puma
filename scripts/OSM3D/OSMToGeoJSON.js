ce6c252dc360fe114dcec9ce472b7c98d9ab05cd
Jakub Balhar <jakub@balhar.net>
2017-10-18 14:02:55 +0200

    Be careful with sidebar

---
+++ scripts/OSM3D/OSMToGeoJSON.js
@@ -0,0 +1,6 @@
+// Script to be run on NodeJS environment. The input is the bounding box. The result is printed in the console and therefore
+// could be simply redirected to any output.
+
+let superagent = require('superagent');
+let osmtogeojson = require('osmtogeojson');
+
