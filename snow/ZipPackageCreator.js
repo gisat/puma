9cbe2ba4c92f4f4e4febabce6f446a03cca971b6
Martin Babic <martin.babic@gisat.cz>
2017-10-23 19:44:41 +0200

    Handle create zip packages

---
+++ snow/ZipPackageCreator.js
@@ -0,0 +1,47 @@
+let JSZip = require('jszip');
+let fs = require('fs');
+
+class ZipPackageCreator {
+    constructor(packagePath) {
+        this._packagePath = packagePath;
+        this._packageObject = ZipPackageCreator.createZipPackageObject();
+    }
+
+    static createZipPackageObject() {
+        return JSZip();
+    }
+
+    addFilesToZipPackage(filePaths, useFullPaths) {
+        for (let filePath of filePaths) {
+            let file = useFullPaths ? filePath : filePath.split('/').pop();
+            this._packageObject.file(
+                file,
+                new Promise((resolve, reject) => {
+                    fs.readFile(filePath, (error, data) => {
+                        if (error) {
+                            reject(error);
+                        } else {
+                            resolve(data);
+                        }
+                    });
+                })
+            );
+        }
+    }
+
+    storeZipPackageToFs() {
+        return new Promise((resolve, reject) => {
+            this._packageObject
+                .generateNodeStream({type: 'nodebuffer', streamFiles: true})
+                .pipe(fs.createWriteStream(this._packagePath))
+                .on('finish', () => {
+                    resolve(`Created zip package at ${this._packagePath}`);
+                })
+                .on('error', (error) => {
+                    reject(error);
+                });
+        });
+    }
+}
+
+module.exports = ZipPackageCreator;
