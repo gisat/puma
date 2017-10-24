3458bc167cd8d9063916957b6fe454122b684a4b
Jakub Balhar <jakub@balhar.net>
2017-10-24 10:38:27 +0200

    Incorrect size of username in the controller.

--- routes/LoginController.js
+++ routes/LoginController.js
@@ -79,7 +79,7 @@ class LoginController {
             response.status(200).json({
                 data: {
                     userId: request.session.userId,
-                    userName: request.session.userName,
+                    userName: request.session.username,
                     groups: request.session.groups
                 },
                 success: true
