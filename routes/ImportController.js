c05128a65cc98c30f87f3d8e42397199a27d0abd
Martin Babic <martin.babic@gisat.cz>
2017-11-30 16:01:18 +0100

    ImportController: refactor & :bug: fixes

--- routes/ImportController.js
+++ routes/ImportController.js
@@ -45,119 +45,204 @@ class ImportController {
 	}
 
 	layer(request, response, next) {
-		request.body.data = JSON.parse(request.body.data);
+		let data = JSON.parse(request.body.data);
+		let place = data.place;
+		let scope = data.scope;
+		let columnMap = data.columnMap ? data.columnMap : {};
+		let files = request.files;
+		let userId = request.session.userId;
+		let isAdmin = response.locals.isAdmin;
 		let systemName = `au_${Math.random().toString(36).substring(3)}`;
-		Promise.resolve().then(() => {
-			let pathToFiles = `${config.temporaryDownloadedFilesLocation}${systemName}`;
 
-			fs.mkdirSync(pathToFiles);
-
-			for (let key of (Object.keys(request.files))) {
-				fs.renameSync(request.files[key].path, `${pathToFiles}/${systemName}.${key}`);
-			}
-
-			return this._geoserverImporter.importLayer(
-				{
-					type: `vector`,
-					systemName: systemName,
-					directory: pathToFiles
-				},
-				true
-			)
-		}).then(() => {
-			return superagent
-				.get(`http://localhost/cgi-bin/updatelayers?f=${systemName}&s=datastore&w=geonode&other=removeDeleted`);
-		}).then(() => {
-			return superagent
-				.get(`http://localhost/geoserver/rest/workspaces/geonode/datastores/datastore/featuretypes/${systemName}.json`)
-				.auth('admin', 'geoserver');
-		}).then(() => {
-			if (!request.body.data.place.hasOwnProperty(`dataset`)) {
-				request.body.data.place[`dataset`] = request.body.data.scope._id;
-			}
-			request.body.data.place.active = true;
-			if (!request.body.data.place.hasOwnProperty(`_id`)) {
-				return new Promise((resolve, reject) => {
-					crud.create(`location`, request.body.data.place, {
-						userId: request.session.userId,
-						isAdmin: response.locals.isAdmin
-					}, (err, result) => {
-						if (err) {
-							reject(new Error("It wasn't possible to create object of type: location by User: " + request.session.userId +
-								" With data: " + request.body.data + " Error: " + err));
-						} else {
-							Promise.all([
-								this.permissions.add(request.session.userId, `location`, result._id, "GET"),
-								this.permissions.add(request.session.userId, `location`, result._id, "PUT"),
-								this.permissions.add(request.session.userId, `location`, result._id, "DELETE")
-							]).then(() => {
-								resolve(result);
-							}).catch((error) => {
-								reject(error);
+		Promise.resolve()
+			.then(() => {
+				if (!place.hasOwnProperty(`dataset`)) {
+					place[`dataset`] = data.scope._id;
+				}
+				if (place.hasOwnProperty(`_id`)) {
+					return this._metadataUpdate(place, `location`, userId, isAdmin);
+				} else {
+					return this._metadataCreate(place, `location`, userId, isAdmin)
+						.then((metadata) => {
+							place = metadata;
+						});
+				}
+			})
+			.then(() => {
+				if (files.shp && files.shx && files.dbf && files.prj) {
+					return this._layerCreate(files, systemName)
+						.then(() => {
+							return this._metadataGet(
+								{
+									location: Number(place._id),
+									isData: false,
+									areaTemplate: Number(scope.featureLayers[0])
+								},
+								`layerref`,
+								userId,
+								isAdmin
+							)
+						})
+						.then((layerrefs) => {
+							return Promise.resolve().then(() => {
+								if (layerrefs.length) {
+									let oldSystemName = layerrefs[0].layer.split(`:`)[1];
+									let promises = [];
+									for (let layerref of layerrefs) {
+										promises.push(
+											this._metadataRemove(
+												{_id: Number(layerref._id)},
+												`layerref`,
+												userId,
+												isAdmin
+											).then(() => {
+												return this._layerRemove(oldSystemName)
+											})
+										);
+									}
+									return Promise.all(promises);
+								}
+							}).then(() => {
+								return this._metadataCreate(
+									{
+										"layer": `geonode:${systemName}`,
+										"location": Number(place._id),
+										"year": null,
+										"active": true,
+										"areaTemplate": Number(scope.featureLayers[0]),
+										"columnMap": [],
+										"isData": false,
+										"fidColumn": "fid",
+										"nameColumn": columnMap[`name`]
+									},
+									`layerref`,
+									userId,
+									isAdmin
+								)
 							});
-						}
-					});
+						});
+				}
+			})
+			.then(() => {
+				response.status(200).send({
+					success: true
 				});
-			} else {
-				return new Promise((resolve, reject) => {
-					crud.update(`location`, object, {
-						userId: request.session.userId,
-						isAdmin: response.locals.isAdmin
-					}, function (err, result) {
-						if (err) {
-							reject(new Error("It wasn't possible to update object of type: location by User: " + request.session.userId +
-								" With data: " + request.body.data + " Error: " + err));
-						} else {
-							resolve(result);
-						}
-					});
+			})
+			.catch((error) => {
+				response.status(500).send({
+					message: error.message,
+					success: false
 				});
-			}
-		}).then((mongoLocation) => {
-			return new Promise((resolve, reject) => {
-				crud.create(
-					`layerref`,
+			});
+	}
+
+	_layerCreate(files, systemName) {
+		return Promise.resolve()
+			.then(() => {
+				let pathToFiles = `${config.temporaryDownloadedFilesLocation}${systemName}`;
+
+				fs.mkdirSync(pathToFiles);
+
+				for (let key of (Object.keys(files))) {
+					fs.renameSync(files[key].path, `${pathToFiles}/${systemName}.${key}`);
+				}
+
+				return this._geoserverImporter.importLayer(
 					{
-						"layer": `geonode:${systemName}`,
-						"location": mongoLocation._id,
-						"year": null,
-						"active": true,
-						"areaTemplate": request.body.data.scope.featureLayers[0],
-						"columnMap": [],
-						"isData": false,
-						"fidColumn": "fid",
-						"nameColumn": "Name",
+						type: `vector`,
+						systemName: systemName,
+						directory: pathToFiles
 					},
-					{
-						userId: request.session.userId,
-						isAdmin: response.locals.isAdmin
-					}, (err, result) => {
-						if (err) {
-							reject(new Error("It wasn't possible to create object of type: location by User: " + request.session.userId +
-								" With data: " + request.body.data + " Error: " + err));
-						} else {
-							Promise.all([
-								this.permissions.add(request.session.userId, `location`, result._id, "GET"),
-								this.permissions.add(request.session.userId, `location`, result._id, "PUT"),
-								this.permissions.add(request.session.userId, `location`, result._id, "DELETE")
-							]).then(() => {
-								resolve(result);
-							}).catch((error) => {
-								reject(error);
-							});
-						}
+					true
+				)
+			})
+			.then(() => {
+				return superagent
+					.get(`http://localhost/cgi-bin/updatelayers?f=${systemName}&s=datastore&w=geonode&other=removeDeleted`);
+			})
+	}
+
+	_layerRemove(systemName) {
+		return this._geoserverImporter.removeVectorLayer(systemName)
+			.then(() => {
+				return superagent
+					.get(`http://localhost/cgi-bin/updatelayers?f=${systemName}&s=datastore&w=geonode&other=removeDeleted`);
+			})
+			.then(() => {
+				return this._pgPool.query(`DROP TABLE IF EXISTS "public"."${systemName}" CASCADE;`);
+			});
+	}
+
+	_metadataCreate(metadata, type, userId, isAdmin) {
+		return new Promise((resolve, reject) => {
+			crud.create(type, metadata, {
+				userId: userId,
+				isAdmin: isAdmin
+			}, (err, result) => {
+				if (err) {
+					reject(new Error("It wasn't possible to create object of type: " + type + " by User: " + userId +
+						" With data: " + metadata + " Error: " + err));
+				} else {
+					Promise.all([
+						this.permissions.add(userId, type, result._id, "GET"),
+						this.permissions.add(userId, type, result._id, "PUT"),
+						this.permissions.add(userId, type, result._id, "DELETE")
+					]).then(() => {
+						resolve(result);
+					}).catch((error) => {
+						reject(error);
 					});
-			});
-		}).then(() => {
-			response.status(200).send({
-				success: true
-			});
-		}).catch((error) => {
-			response.status(500).send({
-				message: error.message,
-				success: false
+				}
 			});
 		});
+	}
+
+	_metadataGet(filter, type, userId, isAdmin) {
+		return new Promise((resolve, reject) => {
+			crud.read(type, filter, {
+				userId: userId,
+				isAdmin: isAdmin
+			}, (err, result) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(result);
+				}
+			});
+		});
+	}
+
+	_metadataUpdate(metadata, type, userId, isAdmin) {
+		metadata = _.cloneDeep(metadata);
+		return new Promise((resolve, reject) => {
+			crud.update(type, metadata, {
+				userId: userId,
+				isAdmin: isAdmin
+			}, function (err, result) {
+				if (err) {
+					reject(new Error("It wasn't possible to update object of type: " + type + " by User: " + userId +
+						" With data: " + JSON.stringify(metadata) + " Error: " + err));
+				} else {
+					resolve(result);
+				}
+			});
+		})
+	}
+
+	_metadataRemove(metadata, type, userId, isAdmin) {
+		return new Promise((resolve, reject) => {
+			crud.remove(type, metadata, {
+				userId: userId,
+				isAdmin: isAdmin
+			}, function (err, result) {
+				if (err) {
+					reject(new Error("It wasn't possible to remove object of type: " + type + " by User: " + userId +
+						" With data: " + metadata + " Error: " + err));
+				} else {
+					resolve(result);
+				}
+			});
+		})
 	}
 }
 
