aa3df512d1624fdbb570ef4cd411fda337b764de
jbalhar <jakub@balhar.net>
2018-02-23 12:44:15 +0100

    Update generation

--- routes/PrintController.js
+++ routes/PrintController.js
@@ -1,20 +1,17 @@
 let logger = require('../common/Logger').applicationWideLogger;
 let GeneratedImage = require('../visualization/GeneratedImage');
-let fs = require('fs');
-let UUID = require('../common/UUID');
 
 class PrintController {
 	constructor(app) {
 		app.post('/print/download/:id', this.download.bind(this));
 		app.get('/print/download/:id', this.download.bind(this));
 		app.get('/print/snapshot/:id', this.snapshot.bind(this));
-
-		app.post('/print/snapshot_url', this.snapshotUrl.bind(this));
 	}
 
-	snapshot(request, response, next) {
+	snapshot(request, response) {
+		logger.info(`PrintController#snapshot Id: ${request.params.id}`);
 		let id = request.params.id;
-		let image = new GeneratedImage(id);
+		let image = new GeneratedImage(id, request.body.url);
 		image.generate().then(function(image){
 			response.set('Content-Type','image/png');
 			response.set('Cache-Control','max-age=60000000');
@@ -22,44 +19,13 @@ class PrintController {
 		});
 	}
 
-    snapshotUrl(request, response) {
-		let url = request.body.url;
-		console.log('PrintController#snapshotUrl' , url);
-		let regex = /^data:.+\/(.+);base64,(.*)$/;
-
-        let matches = url.match(regex);
-        let ext = matches[1];
-        let data = matches[2];
-        let buffer = new Buffer(data, 'base64');
-
-        let filePath = `/tmp/${new UUID().toString()}.${ext}`;
-        console.log(`PrintController#snapshotUrl File`, filePath);
-        fs.writeFile(filePath, buffer, (err) => {
-        	if(err) {
-        		console.error(`PrintController#snapshotUrl Error: `, err);
-			} else {
-                console.log(`PrintController#snapshotUrl Download`);
-                response.download(filePath);
-			}
-		});
-	}
-
-	download(request, response, next) {
-		let id = request.params.id;
+	download(request, response) {
+        logger.info(`PrintController#download Id: ${request.params.id}`);
+        let id = request.params.id;
 		let image = new GeneratedImage(id);
-		image.exists().then(exists => {
-			if(!exists) {
-				image.generate().then(() => {
-					return image.path();
-				}).then(function (path) {
-					response.download(path);
-				});
-			} else {
-				image.path().then(path=>{
-					response.download(path);
-				})
-			}
-		});
+        image.path().then(path=>{
+            response.download(path);
+        });
 	}
 }
 
