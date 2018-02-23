9d379f34d4fcb76262df6f2105eb757567267ba8
jbalhar <jakub@balhar.net>
2018-02-23 10:50:12 +0100

    Allow for sending the data.

--- routes/PrintController.js
+++ routes/PrintController.js
@@ -1,11 +1,15 @@
 let logger = require('../common/Logger').applicationWideLogger;
 let GeneratedImage = require('../visualization/GeneratedImage');
+let fs = require('fs');
+let UUID = require('../common/UUID');
 
 class PrintController {
 	constructor(app) {
 		app.post('/print/download/:id', this.download.bind(this));
 		app.get('/print/download/:id', this.download.bind(this));
 		app.get('/print/snapshot/:id', this.snapshot.bind(this));
+
+		app.post('/print/snapshot_url', this.snapshotUrl.bind(this));
 	}
 
 	snapshot(request, response, next) {
@@ -16,6 +20,21 @@ class PrintController {
 			response.set('Cache-Control','max-age=60000000');
 			response.end(image, 'binary');
 		});
+	}
+
+    snapshotUrl(request, response) {
+		let url = request.body.url;
+        let regex = /^data:.+\/(.+);base64,(.*)$/;
+
+        let matches = url.match(regex);
+        let ext = matches[1];
+        let data = matches[2];
+        let buffer = new Buffer(data, 'base64');
+
+        let filePath = `/tmp/${new UUID().toString()}.${ext}`;
+        fs.writeFileSync(filePath, buffer);
+
+        response.download(filePath);
 	}
 
 	download(request, response, next) {
