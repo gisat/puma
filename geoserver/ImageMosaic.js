8b6ed2d2fa4e44b23a7b6e0bec5d5c00bb8a71bb
Martin Babic <martin.babic@gisat.cz>
2018-03-19 11:57:10 +0100

    ImageMosaic & ImageMosaicController

---
+++ geoserver/ImageMosaic.js
@@ -0,0 +1,118 @@
+const _ = require(`lodash`);
+const fs = require(`fs`);
+const turf = require(`@turf/turf`);
+
+class ImageMosaic {
+	constructor(source, destination) {
+		this._source = source;
+		this._destination = destination;
+	}
+
+	getDatesByGeometry(geometry) {
+		let sourcesFilepath = `${this._destination}/scenes/.sources.json`;
+		let sourcesFile = fs.readFileSync(sourcesFilepath);
+		let sourcesJson = JSON.parse(sourcesFile);
+
+		let dates = [];
+		sourcesJson.sources.forEach(source => {
+			if(turf.booleanOverlap(geometry, source.geometry)) {
+				dates.push(source.acquisition);
+			}
+		});
+
+		return _.uniq(dates);
+	}
+
+
+	prepareImageMosaicFsStructure() {
+		this.deleteFolderRecursive(this._destination);
+		fs.mkdirSync(this._destination);
+		fs.mkdirSync(`${this._destination}/scenes`);
+	}
+
+	prepareImageMosaicMetadata() {
+		let indexer = [
+			`Caching=false`,
+			`TimeAttribute=time`,
+			`Schema=*the_geom:Polygon,location:String,time:java.util.Date`,
+			`PropertyCollectors=TimestampFileNameExtractorSPI[timeregex](time)`
+		];
+		fs.writeFileSync(`${this._destinationPath}/indexer.properties`, indexer.join(`\n`));
+
+		let timeregex = [
+			`regex=[0-9]{8}T[0-9]{6}`
+		];
+		fs.writeFileSync(`${this._destination}/timeregex.properties`, timeregex.join(`\n`));
+	}
+
+	prepareImageMosaicData() {
+		this.prepareImageMosaicFsStructure();
+		this.prepareImageMosaicMetadata();
+		let geojsons = this.getFilesInDirectory(this._source);
+		let sources = [];
+		geojsons.forEach(pathToGeojson => {
+			let geojson = dtdp.getJsonObjectFromFile(pathToGeojson);
+			if (geojson.features[0].geometry) {
+				let pathToSourceTif = pathToGeojson.replace(`.geojson`, `_str.tif`);
+				let fileMetadata = this.getFIleMetadata(pathToSourceTif);
+				if (fs.existsSync(pathToSourceTif)) {
+					let source = {
+						geometry: geojson.features[0].geometry,
+						acquisition: fileMetadata.acquisition
+					};
+					fs.symlinkSync(pathToSourceTif, `${this._destination}/scenes/${fileMetadata.filename}.tif`);
+					sources.push(source);
+					console.log(`Created link to file`, pathToSourceTif);
+				}
+			}
+		});
+		fs.writeFileSync(`${this._destination}/scenes/.sources.json`, JSON.stringify({sources: sources}));
+	}
+
+	getJsonObjectFromFile(path) {
+		return JSON.parse(fs.readFileSync(path));
+	}
+
+	getFilesInDirectory(path, files) {
+		files = files || [];
+		fs.readdirSync(path).forEach((fileName) => {
+			if (fs.statSync(`${path}/${fileName}`).isDirectory()) {
+				this.getFilesInDirectory(`${path}/${fileName}`, files)
+			} else {
+				if (fileName.toLowerCase().endsWith(`.geojson`)) {
+					files.push(`${path}/${fileName}`);
+				}
+			}
+		});
+		return files;
+	}
+
+	getFIleMetadata(path) {
+		let pathParts = path.split(`/`);
+		let filename = pathParts.pop();
+		let code = pathParts.pop();
+		let acquisition = filename.match(/([0-9]{8}T[0-9]{6})/)[0];
+
+		return {
+			filename: `S2_${code}_${acquisition}`,
+			acquisition: acquisition
+		}
+	}
+
+	deleteFolderRecursive(path) {
+		if (path && path !== `/` && fs.existsSync(path)) {
+			fs.readdirSync(path).forEach((file) => {
+				let curPath = path + "/" + file;
+				if (fs.lstatSync(curPath).isDirectory()) {
+					this.deleteFolderRecursive(curPath);
+				} else {
+					console.log(`Removing file ${curPath}`);
+					fs.unlinkSync(curPath);
+				}
+			});
+			fs.rmdirSync(path);
+		}
+	};
+}
+
+module.exports = ImageMosaic;
---
+++ routes/ImageMosaicController.js
@@ -0,0 +1,45 @@
+const Controller = require('./Controller');
+const ImageMosaic = require(`../geoserver/ImageMosaic`);
+
+class ImageMosaicController extends Controller {
+	constructor(app, pool) {
+		super(app, 'imagemosaic');
+
+		this._imageMosaic = new ImageMosaic(
+			`/mnt/pracovni-archiv-01/Sentinel-2/MSI`,
+			`/data/geoserver/dromas-time-serie-data`
+		);
+
+		app.get('/rest/imagemosaic/prepare', this.prepareImageMosaicData.bind(this));
+	}
+
+	readAll(request, response, next) {
+		Promise.resolve(() => {
+			return this._imageMosaic.getDatesByGeometry(geometry);
+		}).then((dates) => {
+			response.status(200).json(
+				{
+					dates: dates
+				}
+			)
+		}).catch((error) => {
+			response.status(500).json(
+				{
+					status: 'err'
+				}
+			)
+		});
+	}
+
+	prepareImageMosaicData(request, response, next) {
+		Promise.resolve(() => {
+			this._imageMosaic.prepareImageMosaicFsStructure();
+			this._imageMosaic.prepareImageMosaicMetadata();
+			this._imageMosaic.prepareImageMosaicData();
+		}).then(() => {
+			response.status(200).json({status: 'ok'});
+		}).catch((error) => {
+			response.status(500).json({status: 'err'});
+		});
+	}
+}
