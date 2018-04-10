113d1b7a931913000931319271a330a8fcb1eb98
jbalhar <jakub@balhar.net>
2018-04-10 09:51:59 +0200

    Update logging.

--- layers/geoserver/GeoServerLayersController.js
+++ layers/geoserver/GeoServerLayersController.js
@@ -37,10 +37,12 @@ class GeoServerLayersController {
 				throw new Error(`No base layers found for given combination.`);
 			}
 			baseLayerIds = layers.map(layer => layer._id);
-			return this._styles.add(new SldStyle(styleId, style));
+            logger.info(`GeoServerLayersController#createLayer Style: ${style}`); // TODO: Deploy and verify.
+            return this._styles.add(new SldStyle(styleId, style));
 		}).then(() => {
 			let promise = Promise.resolve(null);
-			baseLayerIds.forEach(layerId => {
+            logger.info(`GeoServerLayersController#createLayer Layers: ${baseLayerIds.join(',')}, Style Id: ${styleId}`); // TODO: Deploy and verify.
+            baseLayerIds.forEach(layerId => {
 				promise = promise.then(() => {
 					return this._styledLayers.create(`${config.geoserverWorkspace}:layer_${layerId}`, styleId)
 				})
