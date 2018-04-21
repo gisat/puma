9659d8d340b07a14221c3157809f54863e31f549
jbalhar <jakub@balhar.net>
2018-04-21 10:13:41 +0200

    Empty legend SLD when no provided.

--- api/proxy.js
+++ api/proxy.js
@@ -75,7 +75,7 @@ function wms(params, req, res, callback) {
 		if(typeof sld == 'undefined'){
 			console.log("======= SLD ERROR. sldId: ",sldId,"\n\nsldMap:",sldMap,"\n\nsldMapTemp:",sldMapTemp);
 		}
-		params['SLD_BODY'] = params['REQUEST'] == 'GetMap' ? sld.sld : sld.legendSld;
+		params['SLD_BODY'] = params['REQUEST'] == 'GetMap' ? sld.sld : sld && sld.legendSld || '<?xml version="1.0" encoding="UTF-8"?><sld:StyledLayerDescriptor xmlns="http://www.opengis.net/sld" xmlns:sld="http://www.opengis.net/sld" xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc" version="1.0.0"><sld:NamedLayer><sld:Name>Default Styler</sld:Name><sld:UserStyle><sld:Name>Default Styler</sld:Name><sld:FeatureTypeStyle><sld:Name>name</sld:Name></sld:FeatureTypeStyle></sld:UserStyle></sld:NamedLayer></sld:StyledLayerDescriptor>';
 
 		useFirst = false;
 		delete params['STYLES'];
