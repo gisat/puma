29cd5a6ca459d966ec2f863af68bbc8ee0260c9e
Martin Babic <martin.babic@gisat.cz>
2018-04-24 18:43:04 +0200

    UploadManager: initial commit

--- integration/LayerImporterController.js
+++ integration/LayerImporterController.js
@@ -3,17 +3,21 @@ let logger = require('../common/Logger').applicationWideLogger;
 let LayerImporterTasks = require('./LayerImporterTasks');
 let LayerImporter = require('./LayerImporter');
 
+const UploadManager = require(`../integration/UploadManager`);
+
 class LayerImporterController {
-    constructor(app, mongo, pgPool, schema) {
+    constructor(app, mongo, pgPool, schema, pantherUploadStoragePath) {
         app.get('/rest/layerImporter/status/:id', this.getLayerImportStatus.bind(this));
         app.post('/rest/layerImporter/import', this.importLayer.bind(this));
         app.post('/rest/layerImporter/importNoStatistics', this.importNoStatisticsLayer.bind(this));
         app.post('/rest/importer/layer', this.importLayerWithoutMapping.bind(this));
+        app.post('/rest/importer/upload', this.upload.bind(this));
 
         this._mongo = mongo;
         this._pgPool = pgPool;
         this._layerImporterTasks = new LayerImporterTasks();
         this._layerImporter = new LayerImporter(pgPool, mongo, this._layerImporterTasks, schema);
+        this._uploadManager = new UploadManager(pgPool, schema, pantherUploadStoragePath);
     }
 
     /**
@@ -80,6 +84,47 @@ class LayerImporterController {
                 message: error.message,
                 success: false
             })
+        });
+    }
+
+	/**
+     * Just upload file (vector, raster etc) and store it in filesystem.
+	 * @param request
+	 * @param response
+	 */
+	upload(request, response) {
+		this.getUploadInputs(request)
+			.then((uploadInputs) => {
+				return this._uploadManager.add(uploadInputs);
+			})
+			.then((metadata) => {
+				response.status(200).json({
+					data: metadata,
+					success: true
+				});
+			})
+			.catch((error) => {
+				response.status(500).json({
+					message: error.message,
+					success: false
+				})
+			})
+    }
+
+	/**
+	 * Return parsed upload inputs from request
+	 * @param request
+	 */
+	getUploadInputs(request) {
+	    return Promise.resolve().then(() => {
+	        return {
+	        	path: request.files.file.path,
+				name: request.files.file.originalFilename || request.files.file.name,
+				customName: request.body.name,
+				user: {
+					id: request.session.user.id
+				}
+			}
         });
     }
 
