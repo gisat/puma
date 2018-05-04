b5ab66b95671701d9223c64eddc044e8055e663e
Martin Babic <martin.babic@gisat.cz>
2018-05-04 18:43:28 +0200

    PgScenarios, PgScenarioCases & theirs controllers - initial commit

---
+++ metadata/PgScenarios.js
@@ -0,0 +1,73 @@
+const _ = require('lodash');
+
+const PgCollection = require('../common/PgCollection');
+const PgScenarioScenarioCaseRelations = require('./PgScenarioScenarioCaseRelations');
+
+class PgScenarios extends PgCollection {
+	constructor(pgPool, pgSchema) {
+		super(pgPool, pgSchema, 'PgScenarios');
+
+		this._pgScenarioScenarioCaseRelations = new PgScenarioScenarioCaseRelations(pgPool, pgSchema);
+	}
+
+	createWithouId(object) {
+		if (!object) throw new Error('empty input');
+
+		let scenarioCaseId;
+		if(object.hasOwnProperty('scenario_case_id')) {
+			scenarioCaseId = object['scenario_case_id'];
+			delete object['scenario_case_id'];
+		}
+
+		let keys = Object.keys(object);
+		let columns = _.map(keys, (key) => {
+			return `"${key}"`;
+		});
+		let values = _.map(keys, (key) => {
+			return _.isNumber(object[key]) ? object[key] : `'${object[key]}'`;
+		});
+
+		return this._pool.query(`INSERT INTO "${this._schema}"."${PgScenarios.tableName()}" (${columns.join(', ')}) VALUES (${values.join(', ')}) RETURNING *;`)
+			.then((queryResult) => {
+				if (queryResult.rowCount) {
+					return queryResult.rows[0];
+				} else {
+					throw new Error('insert failed');
+				}
+			})
+			.then((createdObject) => {
+				if(scenarioCaseId) {
+					return this._pgScenarioScenarioCaseRelations.createWithouId({
+						scenario_case_id: scenarioCaseId,
+						scenario_id: createdObject.id
+					}).then(() => {
+						createdObject['scenario_case_id'] = scenarioCaseId;
+						return createdObject;
+					});
+				} else {
+					return createdObject;
+				}
+			});
+	}
+
+	getFiltered(filter) {
+		let keys = filter ? Object.keys(filter) : [];
+		let columns = _.map(keys, (key) => {
+			return `"${key}"`;
+		});
+		let values = _.map(keys, (key) => {
+			return _.isNumber(filter[key]) ? filter[key] : `'${filter[key]}'`;
+		});
+
+		return this._pool.query(`SELECT * FROM "${this._schema}"."${PgScenarios.tableName()}";`)
+			.then((queryResult) => {
+				return queryResult.rows;
+			});
+	}
+
+	static tableName() {
+		return `scenario`;
+	}
+}
+
+module.exports = PgScenarios;
---
+++ routes/PgScenariosController.js
@@ -0,0 +1,46 @@
+const PgScenarios = require('../metadata/PgScenarios');
+
+class PgScenariosController {
+	constructor(app, pgPool, pgSchema) {
+		this._pgScenarios = new PgScenarios(pgPool, pgSchema);
+
+		app.post(`/rest/metadata/scenarios/create`, this.create.bind(this));
+
+		app.get(`/rest/metadata/scenarios/get`, this.get.bind(this));
+		app.get(`/rest/metadata/scenarios/get/:scenario_case_id`, this.get.bind(this));
+	}
+
+	create(request, response) {
+		this._pgScenarios.createWithouId(request.body)
+			.then((scenario) => {
+				response.status(200).json({
+					data: scenario,
+					success: true
+				})
+			})
+			.catch((error) => {
+				response.status(500).json({
+					message: error.message,
+					success: false
+				})
+			});
+	}
+
+	get(request, response) {
+		this._pgScenarios.getFiltered(request.params)
+			.then((results) => {
+				response.status(200).json({
+					data: results,
+					success: true
+				})
+			})
+			.catch((error) => {
+				response.status(500).json({
+					message: error.message,
+					success: false
+				});
+			});
+	}
+}
+
+module.exports = PgScenariosController;
