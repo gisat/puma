2380f5c4541ba7ef6f5d634bf57fbefb8ffb8dcb
Martin Babic <martin.babic@gisat.cz>
2018-05-10 17:47:05 +0200

    PgPlaceScenarioCaseRelations: relation between ScenarioCase & Place

---
+++ metadata/PgPlaceScenarioCaseRelations.js
@@ -0,0 +1,51 @@
+const _ = require('lodash');
+
+const PgCollection = require('../common/PgCollection');
+
+class PgPlaceScenarioCaseRelations extends PgCollection {
+	constructor(pgPool, pgSchema) {
+		super(pgPool, pgSchema, 'PgScenarios');
+	}
+
+	create(object) {
+		if (!object) throw new Error('empty input');
+
+		let keys = Object.keys(object);
+		let columns = _.map(keys, (key) => {
+			return `"${key}"`;
+		});
+		let values = _.map(keys, (key) => {
+			return _.isNumber(object[key]) ? object[key] : `'${object[key]}'`;
+		});
+
+		return this._pool.query(`INSERT INTO "${this._schema}"."${PgPlaceScenarioCaseRelations.tableName()}" (${columns.join(', ')}) VALUES (${values.join(', ')}) RETURNING *;`)
+			.then((queryResult) => {
+				if (queryResult.rowCount) {
+					return queryResult.rows[0];
+				} else {
+					throw new Error('insert failed');
+				}
+			});
+	}
+
+	getFiltered(filter) {
+		let keys = filter ? Object.keys(filter) : [];
+		let columns = _.map(keys, (key) => {
+			return `"${key}"`;
+		});
+		let values = _.map(keys, (key) => {
+			return _.isNumber(filter[key]) ? filter[key] : `'${filter[key]}'`;
+		});
+
+		return this._pool.query(`SELECT * FROM "${this._schema}"."${PgPlaceScenarioCaseRelations.tableName()}";`)
+			.then((queryResult) => {
+				return queryResult.rows;
+			});
+	}
+
+	static tableName() {
+		return `place_scenario_case_relation`;
+	}
+}
+
+module.exports = PgPlaceScenarioCaseRelations;
