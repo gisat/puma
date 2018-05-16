042140e386f8e9923ec72748b28e71dce88e5bf2
Martin Babic <martin.babic@gisat.cz>
2018-05-16 18:56:06 +0200

    PgScenario & PgScenarioCases: allow update and delete records

--- routes/PgScenarioCasesController.js
+++ routes/PgScenarioCasesController.js
@@ -4,10 +4,14 @@ class PgScenarioCasesController {
 	constructor(app, pgPool, pgSchema) {
 		this._pgScenarioCases = new PgScenarioCases(pgPool, pgSchema);
 
-		app.post(`/rest/metadata/scenario_cases/create`, this.create.bind(this));
+		app.post(`/rest/metadata/scenario_cases`, this.create.bind(this));
 
-		app.get(`/rest/metadata/scenario_cases/get`, this.get.bind(this));
-		app.post(`/rest/metadata/scenario_cases/get`, this.get.bind(this));
+		app.get(`/rest/metadata/scenario_cases`, this.get.bind(this));
+		app.post(`/rest/metadata/scenario_cases/filtered`, this.get.bind(this));
+
+		app.delete(`/rest/metadata/scenario_cases/:id`, this.delete.bind(this));
+
+		app.put(`/rest/metadata/scenario_cases/:id`, this.update.bind(this))
 	}
 
 	create(request, response) {
@@ -27,10 +31,39 @@ class PgScenarioCasesController {
 	}
 
 	get(request, response) {
-		this._pgScenarioCases.getFiltered(request.body)
+		this._pgScenarioCases.get(request.body)
 			.then((payload) => {
 				payload.success = true;
 				response.status(200).json(payload)
+			})
+			.catch((error) => {
+				response.status(500).json({
+					message: error.message,
+					success: false
+				});
+			});
+	}
+
+	delete(request, response) {
+		this._pgScenarioCases.delete(request.params.id || request.body.id)
+			.then(() => {
+				response.status(200).json({
+					success: true
+				});
+			})
+			.catch((error) => {
+				response.status(500).json({
+					message: error.message,
+					success: false
+				});
+			});
+	}
+
+	update(request, response) {
+		this._pgScenarioCases.update(request.params.id || request.body.id, request.body)
+			.then((payload) => {
+				payload.success = true;
+				response.status(200).json(payload);
 			})
 			.catch((error) => {
 				response.status(500).json({
--- routes/PgScenariosController.js
+++ routes/PgScenariosController.js
@@ -4,10 +4,14 @@ class PgScenariosController {
 	constructor(app, pgPool, pgSchema) {
 		this._pgScenarios = new PgScenarios(pgPool, pgSchema);
 
-		app.post(`/rest/metadata/scenarios/create`, this.create.bind(this));
+		app.post(`/rest/metadata/scenarios`, this.create.bind(this));
 
-		app.get(`/rest/metadata/scenarios/get`, this.get.bind(this));
-		app.post(`/rest/metadata/scenarios/get`, this.get.bind(this));
+		app.get(`/rest/metadata/scenarios`, this.get.bind(this));
+		app.post(`/rest/metadata/scenarios/filtered`, this.get.bind(this));
+
+		app.delete(`/rest/metadata/scenarios/:id`, this.delete.bind(this));
+
+		app.put(`/rest/metadata/scenarios/:id`, this.update.bind(this))
 	}
 
 	create(request, response) {
@@ -27,10 +31,39 @@ class PgScenariosController {
 	}
 
 	get(request, response) {
-		this._pgScenarios.getFiltered(request.body)
+		this._pgScenarios.get(request.body)
 			.then((payload) => {
 				payload.success = true;
 				response.status(200).json(payload)
+			})
+			.catch((error) => {
+				response.status(500).json({
+					message: error.message,
+					success: false
+				});
+			});
+	}
+
+	delete(request, response) {
+		this._pgScenarios.delete(request.params.id || request.body.id)
+			.then(() => {
+				response.status(200).json({
+					success: true
+				});
+			})
+			.catch((error) => {
+				response.status(500).json({
+					message: error.message,
+					success: false
+				});
+			});
+	}
+
+	update(request, response) {
+		this._pgScenarios.update(request.params.id || request.body.id, request.body)
+			.then((payload) => {
+				payload.success = true;
+				response.status(200).json(payload);
 			})
 			.catch((error) => {
 				response.status(500).json({
