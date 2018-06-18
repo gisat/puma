21b758ebc6fd5d9caa7b627eb7f9e9beadb282a1
Martin Babic <martin.babic@gisat.cz>
2018-06-18 21:39:43 +0200

    DataLayerDuplicatorSpec: tests for layer duplication over rest

--- test/functional/layers/DataLayerDuplicatorSpec.js
+++ test/functional/layers/DataLayerDuplicatorSpec.js
@@ -2,12 +2,16 @@ const supertest = require('supertest');
 const should = require('should');
 const expect = require('chai').expect;
 const _ = require('lodash');
+const express = require('express');
 
-let logger = require('../../../common/Logger').applicationWideLogger;
+const config = require('../config');
 
-let IntegrationEnvironment = require('../IntegrationEnvironment');
+const logger = require('../../../common/Logger').applicationWideLogger;
 
-let DataLayerDuplicator = require('../../../layers/DataLayerDuplicator');
+const IntegrationEnvironment = require('../IntegrationEnvironment');
+
+const DataLayerDuplicator = require('../../../layers/DataLayerDuplicator');
+const LayerImporterController = require('../../../integration/LayerImporterController');
 
 describe('DataLayerDuplicator', () => {
 	let dataLayerDuplicator = new DataLayerDuplicator();
@@ -29,7 +33,7 @@ describe('DataLayerDuplicator', () => {
 					return dataLayerDuplicator.downloadFileFromRemote(url);
 				})
 				.then((result) => {
-					if(!_.isObject(result) || !result.hasOwnProperty('contentType')) {
+					if (!_.isObject(result) || !result.hasOwnProperty('contentType')) {
 						throw new Error('Expected object with contentType key');
 					} else {
 						done();
@@ -45,7 +49,7 @@ describe('DataLayerDuplicator', () => {
 		it('...', (done) => {
 			dataLayerDuplicator.duplicateGeoserverLayer('geonode:pucs_192351e289bf4c56a83fe700400771ae')
 				.then((layerName) => {
-					if(!layerName.startsWith('panther_')) throw new Error('Duplication failed...');
+					if (!layerName.startsWith('panther_')) throw new Error('Duplication failed...');
 					done();
 				})
 				.catch((error) => {
@@ -53,5 +57,67 @@ describe('DataLayerDuplicator', () => {
 					done(error);
 				})
 		}).timeout(60000);
+	});
+
+	describe('Duplicate geoserver layer using rest', () => {
+		let integrationEnviroment;
+
+		before((done) => {
+			integrationEnviroment = new IntegrationEnvironment((app, pool, schema, mongoDb) => {
+				new LayerImporterController(app, mongoDb, pool, schema.schema, config.pantherDataStoragePath);
+			}, {user: null});
+
+			integrationEnviroment
+				.setup()
+				.then(() => {
+					done();
+				})
+				.catch((error) => {
+					done(error);
+				})
+		});
+
+		after((done) => {
+			integrationEnviroment
+				.tearDown()
+				.then(() => {
+					done();
+				})
+		});
+
+		it('Duplicate layer...', (done) => {
+			supertest(integrationEnviroment.app)
+				.post('/rest/importer/duplicate')
+				.set('Content-Type', 'application/json')
+				.set('Accepts', 'application/json')
+				.send({data: {layerName: "geonode:pucs_192351e289bf4c56a83fe700400771ae"}})
+				.then((response) => {
+					if(!response.body.data || !response.body.data.uuid) throw new Error('Unexpected response');
+					return response.body.data.uuid;
+				})
+				.then((processUuid) => {
+					let interval = setInterval(() => {
+						supertest(integrationEnviroment.app)
+							.get(`/rest/importer/duplicate/${processUuid}`)
+							.set('Content-Type', 'application/json')
+							.set('Accepts', 'application/json')
+							.then((response) => {
+								if(!response.body.data || !response.body.data.uuid) {
+									throw new Error('Unexpected response');
+								}
+								if(response.body.data.status === 'done') {
+									clearInterval(interval);
+									done();
+								}
+							})
+							.catch((error) => {
+								done(error);
+							})
+					}, 1000);
+				})
+				.catch((error) => {
+					done(error);
+				});
+		}).timeout(90000);
 	});
 });
