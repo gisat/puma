021f5e151005d900e496c2736e619b0964b5bcad
Martin Babic <martin.babic@gisat.cz>
2018-06-19 18:08:34 +0200

    PucsMatlabProcessorController: calculate model for remote & local geoserver layer

--- integration/PucsMatlabProcessorController.js
+++ integration/PucsMatlabProcessorController.js
@@ -1,3 +1,5 @@
+const hash = require('object-hash');
+
 const config = require('../config');
 
 const CalculatePragueTemperatureMapUsingNeuralNetworkModel = require('../wps/processes/CalculatePragueTemperatureMapUsingNeuralNetworkModel');
@@ -12,41 +14,65 @@ class PucsMatlabProcessorController {
 	}
 
 	executeMatlabProcessor(request, response) {
-		let uploadKey = request.body.uploadKey;
-		if(uploadKey) {
-			if(!processes[uploadKey]) {
-				processes[uploadKey] = {
-					status: "running"
-				};
+		if(request.body.data) {
+			let uploadKey = request.body.data.uploadKey;
+			let remotePath = request.body.data.remotePath;
+			let localLayer = request.body.data.localLayer;
+			let processKey = hash(uploadKey || remotePath || localLayer);
 
-				this._calculatePragueTemperatureMapUsingNeuralNetworkModel.execute({
-					dataInputs:
-						[ { identifier: 'inputFile',
-							data: `upload:${uploadKey}` } ],
-					owner: request.session.user
-				}).then((result) => {
-					processes[uploadKey] = {
-						status: "done",
-						result: result[0].data
+			if(uploadKey || remotePath || localLayer) {
+				if(!processes[processKey]) {
+					processes[processKey] = {
+						status: "running"
 					};
-				}).catch((error) => {
-					processes[uploadKey] = {
-						status: "error",
-						message: error.message
+
+					let type, identifier;
+					if(uploadKey) {
+						type = "uploadKey";
+						identifier  = uploadKey;
+					} else if(remotePath) {
+						type = "remotePath";
+						identifier = remotePath;
+					} else if (localLayer) {
+						type = "localLayer";
+						identifier = localLayer;
 					}
-				});
-			}
-			if(processes[uploadKey]['status'] === "error") {
-				response.status(500).json({
-					message: processes[uploadKey]['message'],
-					success: false
-				});
+
+					this._calculatePragueTemperatureMapUsingNeuralNetworkModel.execute({
+						dataInputs:
+							[ { identifier: 'inputFile',
+								data: `${type}:${identifier}` } ],
+						owner: request.session.user
+					}).then((result) => {
+						processes[processKey] = {
+							status: "done",
+							result: result[0].data
+						};
+					}).catch((error) => {
+						console.log(error);
+						processes[processKey] = {
+							status: "error",
+							message: error.message
+						}
+					});
+				}
+				if(processes[processKey]['status'] === "error") {
+					response.status(500).json({
+						message: processes[processKey]['message'],
+						success: false
+					});
+				} else {
+					response.status(200).json(processes[processKey]);
+				}
 			} else {
-				response.status(200).json(processes[uploadKey]);
+				response.status(500).json({
+					message: "missing uploadKey, remotePath or localLayer",
+					success: false
+				})
 			}
 		} else {
 			response.status(500).json({
-				message: "unknown upload key",
+				message: "no data",
 				success: false
 			})
 		}
