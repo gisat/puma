d54cdcab11bf54464701c2a9f10d414231db0291
Martin Babic <martin.babic@gisat.cz>
2018-06-26 17:54:51 +0200

    PucsMatlabProcessorControllerSpec: test for /rest/pucs/publish

--- test/functional/integration/PucsMatlabProcessorControllerSpec.js
+++ test/functional/integration/PucsMatlabProcessorControllerSpec.js
@@ -5,6 +5,117 @@ const logger = require('../../../common/Logger').applicationWideLogger;
 
 const IntegrationEnvironment = require('../IntegrationEnvironment');
 const PucsMatlabProcessorController = require('../../../integration/PucsMatlabProcessorController');
+const MongoScopes = require('../../../metadata/MongoScopes');
+
+describe('Publish matlab results', () => {
+	let integrationEnviroment;
+
+	before((done) => {
+		integrationEnviroment = new IntegrationEnvironment((app, pool, schema, mongoDb) => {
+			new PucsMatlabProcessorController(app, pool, schema.schema, mongoDb);
+			new MongoScopes(mongoDb).add({
+				"_id": 3971,
+				"active": true,
+				"createdBy": 1,
+				"changedBy": 1,
+				"name": "PUCS_test",
+				"featureLayers": [3968],
+				"years": [3969],
+				"isMapIndependentOfPeriod": true,
+				"viewSelection": "placeSelector",
+				"hideSidebarReports": true,
+				"showTimeline": false,
+				"layersWidgetHiddenPanels": ["thematic-layers", "wms-layers"],
+				"removedTools": ["2dmap", "areas", "selections", "snapshot", "contextHelp", "visualization", "scope", "theme", "place", "period", "visualization-always", "areasFilter", "customLayers"],
+				"scenarios": true,
+				"isMapDependentOnScenario": true,
+				"configuration": {
+					"pucsLandUseScenarios": {
+						"templates": {
+							"sourceVector": 3332,
+							"sourceRaster": 4090,
+							"uhi": 4091,
+							"hwd": 4092
+						}
+					}
+				}
+			})
+		}, {user: {id: 0}});
+
+		integrationEnviroment
+			.setup()
+			.then(() => {
+				done();
+			})
+			.catch((error) => {
+				done(error);
+			})
+	});
+
+	after((done) => {
+		integrationEnviroment
+			.tearDown()
+			.then(() => {
+				done();
+			})
+	});
+
+	it('...', (done) => {
+		let interval, execute = () => {
+			supertest(integrationEnviroment.app)
+				.post('/rest/pucs/publish')
+				.set('Content-Type', 'application/json')
+				.set('Accepts', 'application/json')
+				.send(
+					{
+						data: [
+							{
+								uuid: "s5f4s6df54sd6fdsfs6fs6d",
+								data: {
+									localLayer: `geonode:pucs_192351e289bf4c56a83fe700400771ae`,
+									scope_id: 3971,
+									place_id: 1,
+									scenario_id: 1
+								}
+							}
+						]
+					}
+				)
+				.then((response) => {
+					console.log(JSON.stringify(response.body));
+
+					if (response.status === 200) {
+						if (response.body.data) {
+							let allDone = true;
+
+							response.body.data.forEach((data) => {
+								if (data.status === "running") {
+									allDone = false;
+								}
+							});
+
+							if (allDone) {
+								clearInterval(interval);
+								done();
+							}
+						}
+					} else if (response.status === 500) {
+						clearInterval(interval);
+						done();
+					}
+				})
+				.catch((error) => {
+					done(error);
+				});
+		};
+
+		interval = setInterval(() => {
+			execute();
+		}, 2000);
+
+		execute();
+	}).timeout(180000);
+});
 
 describe('Execute pucs matlab processor on local geoserver layer', () => {
 	let integrationEnviroment;
@@ -43,7 +154,7 @@ describe('Execute pucs matlab processor on local geoserver layer', () => {
 					console.log(response.body);
 
 					if (response.status === 200) {
-						if(response.body.result) {
+						if (response.body.result) {
 							clearInterval(interval);
 							done();
 						}
@@ -102,7 +213,7 @@ describe('Execute pucs matlab processor on remote geoserver layer', () => {
 					console.log(response.body);
 
 					if (response.status === 200) {
-						if(response.body.result) {
+						if (response.body.result) {
 							clearInterval(interval);
 							done();
 						}
