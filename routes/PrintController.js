f0c0c4332779826699e3a6e992d2aa18c9de4af3
Jakub Balhar <jakub@balhar.net>
2018-11-20 13:19:41 +0100

    Provide the URL to the display

--- routes/PrintController.js
+++ routes/PrintController.js
@@ -1,11 +1,13 @@
-let logger = require('../common/Logger').applicationWideLogger;
-let GeneratedImage = require('../visualization/GeneratedImage');
+const logger = require('../common/Logger').applicationWideLogger;
+const GeneratedImage = require('../visualization/GeneratedImage');
+const fs = require('fs');
 
 class PrintController {
 	constructor(app) {
         app.post('/print/snapshot/:id', this.snapshot.bind(this));
 
 		app.get('/print/download/:id', this.download.bind(this));
+        app.get('/print/display/:id', this.download.bind(this));
 	}
 
 	snapshot(request, response) {
@@ -19,6 +21,19 @@ class PrintController {
 			});
 		});
 	}
+
+	display(request, response) {
+        logger.info(`PrintController#display Id: ${request.params.id}`);
+        let id = request.params.id;
+        let image = new GeneratedImage(id, request.body.url);
+        image.path().then(path => {
+            response.set('Content-Type','image/png');
+            response.set('Cache-Control','max-age=60000000');
+
+            const src = fs.createReadStream(path);
+            src.pipe(response);
+        });
+    }
 
 	download(request, response) {
         logger.info(`PrintController#download Id: ${request.params.id}`);
