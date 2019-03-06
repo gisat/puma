674b6fa645b621410bdbb22810856cb6902140e9
Jakub Balhar <jakub@balhar.net>
2019-03-06 20:34:41 +0100

    Clustering for the Server
    This way the server should be running on multiple CPUs and not just one.

---
+++ cluster.js
@@ -0,0 +1,13 @@
+var cluster = require('cluster');
+
+if(cluster.isMaster) {
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
+
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+} else {
+    require('./server');
+}
