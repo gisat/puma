a9009cc6ff07e656a11c1c068f1f8bd1840137a6
Jakub Balhar <jakub@balhar.net>
2019-03-25 09:33:43 +0100

    Update throwing in the initial data view creation.

--- visualization/MongoDataViews.js
+++ visualization/MongoDataViews.js
@@ -67,7 +67,7 @@ class MongoDataViews {
             logger.info(`MongoDataViews#defaultForScope LayerReferences: ${layerReferences.length}`, layerReferences);
             if(layerReferences.length === 0) {
                 logger.error(`MongoDataViews#defaultForScope Nonexistent base layer for analyticalUnitLevel: ${analyticalUnitLevel} Location: ${location}, Year: ${year}.`);
-                throw 'There is no mapped data layer for the top analytical unit level.';
+                throw new Error('There is no mapped data layer for the top analytical unit level.');
             }
 
             if(layerReferences.length > 1) {
@@ -82,7 +82,7 @@ class MongoDataViews {
             logger.info(`MongoDataViews#defaultForScope LayerReferences: ${analyticalUnits.length}`, analyticalUnits);
             if(analyticalUnits.length === 0) {
                 logger.error(`MongoDataViews#defaultForScope Base layer doesn't contain any unit for analyticalUnitLevel: ${analyticalUnitLevel} Location: ${location}, Year: ${year}`);
-                throw `Base layer doesn't contain any unit for analyticalUnitLevel: ${analyticalUnitLevel} Location: ${location}, Year: ${year}`;
+                throw new Error(`Base layer doesn't contain any unit for analyticalUnitLevel: ${analyticalUnitLevel} Location: ${location}, Year: ${year}`);
             }
 
             var dataView = {
