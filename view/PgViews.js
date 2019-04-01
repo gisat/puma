15d2df651a695f3c32d2be0dadb17c8f66329147
Martin Babič <martin.babic@gisat.cz>
2019-04-01 12:53:42 +0200

    PgViews: rename column data to state & add some new columns

--- view/PgViews.js
+++ view/PgViews.js
@@ -22,7 +22,10 @@ class PgViews extends PgCollection {
 		CREATE TABLE IF NOT EXISTS "${this._pgSchema}"."${this._tableName}" (
 			"key" UUID PRIMARY KEY DEFAULT gen_random_uuid()
 		);
-		ALTER TABLE "${this._pgSchema}"."${this._tableName}" ADD COLUMN IF NOT EXISTS "data" JSONB;
+		ALTER TABLE "${this._pgSchema}"."${this._tableName}" ADD COLUMN IF NOT EXISTS "nameInternal" TEXT;
+		ALTER TABLE "${this._pgSchema}"."${this._tableName}" ADD COLUMN IF NOT EXISTS "nameDisplay" TEXT;
+		ALTER TABLE "${this._pgSchema}"."${this._tableName}" ADD COLUMN IF NOT EXISTS "description" TEXT;
+		ALTER TABLE "${this._pgSchema}"."${this._tableName}" ADD COLUMN IF NOT EXISTS "state" JSONB;
 		COMMIT;
 		`;
 	}
