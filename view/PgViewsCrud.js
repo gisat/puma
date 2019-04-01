2de046c7f89cf6f523e840ccd66626b9077a47e2
Martin Babič <martin.babic@gisat.cz>
2019-04-01 16:13:21 +0200

    Add application to view relation

--- view/PgViewsCrud.js
+++ view/PgViewsCrud.js
@@ -1,12 +1,20 @@
+const config = require(`../config`);
+
 const PgCrud = require(`../common/PgCrud`);
 
 const PgViews = require(`./PgViews`);
+
+const PgApplications = require(`../application/PgApplications`);
 
 class PgViewsCrud extends PgCrud {
 	constructor(pgPool, pgSchema) {
 		super();
 
 		this._pgViews = new PgViews(pgPool, pgSchema);
+
+		this._pgApplications = new PgApplications(pgPool, config.pgSchema.application);
+
+		this._pgViews.setRelatedStores([this._pgApplications]);
 
 		this._pgTypes = {
 			[PgViews.groupName()]: {
