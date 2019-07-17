6bbd946477b4b644aa3f73afa96b58cc595ff2d6
Jakub Balhar <jakub@balhar.net>
2019-07-17 13:00:10 +0200

    Updated handling of LineString and MultiLineStrings

--- integration/lulc/TransportationNetwork.js
+++ integration/lulc/TransportationNetwork.js
@@ -35,57 +35,14 @@ class TransportationNetwork {
                 });
 
                 this._transportationVectorLayer.features.forEach(line => {
-                    line.geometry.coordinates.forEach(part => {
-                        // It differs with LineString and MultiLineString
-                        try {
-                            const lineStringResult = turf.lineString(part);
-                            if (!lineStringResult.bbox) {
-                                lineStringResult.bbox = turf.bbox(lineStringResult);
-                            }
-                            let split = turf.lineSplit(lineStringResult, analyticalUnit);
-                            // If the split returns empty array then either both parts are outside or inside the polygon
-                            if (split.features.length === 0) {
-                                // If it is inside take the whole length;
-                                if (turf.booleanPointInPolygon(turf.point(part[0]), analyticalUnit) && turf.booleanPointInPolygon(turf.point(part[part.length - 1]), analyticalUnit)) {
-                                    const lengthOfLine = turf.length(turf.lineString(part));
-                                    const usedCodes = [];
-                                    this._attributes.forEach(attribute => {
-                                        const codeOfPolygon = line.properties[attribute.columnName];
-                                        if (typeof attributesAreas[codeOfPolygon] !== 'undefined' && usedCodes.indexOf(codeOfPolygon) === -1) {
-                                            attributesAreas[codeOfPolygon].area += lengthOfLine;
-                                            usedCodes.push(codeOfPolygon);
-                                        }
-                                    });
-                                    attributesAreas['total'].area += lengthOfLine;
-                                }
-                            } else {
-                                // This decides whether odd or even parts of the Line String are in the polygon.
-                                let oddPair;
-                                if (turf.booleanPointInPolygon(turf.point(part[0]), analyticalUnit)) {
-                                    oddPair = 0;
-                                } else {
-                                    oddPair = 1;
-                                }
-
-                                split.features.forEach((splitPart, i) => {
-                                    if ((i + oddPair) % 2 === 0) {
-                                        const lengthOfLine = turf.length(splitPart);
-                                        const usedCodes = [];
-                                        this._attributes.forEach(attribute => {
-                                            const codeOfPolygon = line.properties[attribute.columnName];
-                                            if (typeof attributesAreas[codeOfPolygon] !== 'undefined' && usedCodes.indexOf(codeOfPolygon) === -1) {
-                                                attributesAreas[codeOfPolygon].area += lengthOfLine;
-                                                usedCodes.push(codeOfPolygon);
-                                            }
-                                        });
-                                        attributesAreas['total'].area += lengthOfLine;
-                                    }
-                                });
-                            }
-                        } catch (err) {
-                            console.log('ERROR ', err, ' ', part);
-                        }
-                    });
+                    if(line.geometry.type === "MultiLineString") {
+                        line.geometry.coordinates.forEach(part => {
+                            // It differs with LineString and MultiLineString
+                            this.handleCoordinatesForOneLineString(part, line, analyticalUnit, attributesAreas);
+                        });
+                    } else {
+                        this.handleCoordinatesForOneLineString(line.geometry.coordinates, line, analyticalUnit, attributesAreas);
+                    }
                 });
 
                 console.log(attributesAreas);
@@ -109,6 +66,57 @@ class TransportationNetwork {
         });
 
         return this._areasVectorLayer;
+    }
+
+    handleCoordinatesForOneLineString(part, line, analyticalUnit, attributesAreas) {
+        try {
+            const lineStringResult = turf.lineString(part);
+            if (!lineStringResult.bbox) {
+                lineStringResult.bbox = turf.bbox(lineStringResult);
+            }
+            let split = turf.lineSplit(lineStringResult, analyticalUnit);
+            // If the split returns empty array then either both parts are outside or inside the polygon
+            if (split.features.length === 0) {
+                // If it is inside take the whole length;
+                if (turf.booleanPointInPolygon(turf.point(part[0]), analyticalUnit) && turf.booleanPointInPolygon(turf.point(part[part.length - 1]), analyticalUnit)) {
+                    const lengthOfLine = turf.length(turf.lineString(part));
+                    const usedCodes = [];
+                    this._attributes.forEach(attribute => {
+                        const codeOfPolygon = line.properties[attribute.columnName];
+                        if (typeof attributesAreas[codeOfPolygon] !== 'undefined' && usedCodes.indexOf(codeOfPolygon) === -1) {
+                            attributesAreas[codeOfPolygon].area += lengthOfLine;
+                            usedCodes.push(codeOfPolygon);
+                        }
+                    });
+                    attributesAreas['total'].area += lengthOfLine;
+                }
+            } else {
+                // This decides whether odd or even parts of the Line String are in the polygon.
+                let oddPair;
+                if (turf.booleanPointInPolygon(turf.point(part[0]), analyticalUnit)) {
+                    oddPair = 0;
+                } else {
+                    oddPair = 1;
+                }
+
+                split.features.forEach((splitPart, i) => {
+                    if ((i + oddPair) % 2 === 0) {
+                        const lengthOfLine = turf.length(splitPart);
+                        const usedCodes = [];
+                        this._attributes.forEach(attribute => {
+                            const codeOfPolygon = line.properties[attribute.columnName];
+                            if (typeof attributesAreas[codeOfPolygon] !== 'undefined' && usedCodes.indexOf(codeOfPolygon) === -1) {
+                                attributesAreas[codeOfPolygon].area += lengthOfLine;
+                                usedCodes.push(codeOfPolygon);
+                            }
+                        });
+                        attributesAreas['total'].area += lengthOfLine;
+                    }
+                });
+            }
+        } catch (err) {
+            console.log('ERROR ', err, ' ', part);
+        }
     }
 }
 
