773f62a97b104759c28cdb3cf9a082129f5be79d
Martin Babič <martin.babic@gisat.cz>
2020-05-21 11:33:50 +0200

    Routes.js init

---
+++ routes/Routes.js
@@ -0,0 +1,31 @@
+const PgRelationsController = require('../relations/PgRelationsController');
+const PgMetadataController = require('../metadata/PgMetadataController');
+const PgUserController = require('../user/PgUserController');
+const PgApplicationController = require('../application/PgApplicationsController');
+const PgSpecificController = require('../specific/PgSpecificController');
+const PgDataSourcesController = require(`../dataSources/PgDataSourcesController`);
+const PgViewsController = require(`../view/PgViewsController`);
+const PgDataController = require(`../data/PgDataController`);
+
+const config = require(`../config`);
+
+class Routes {
+	constructor(app, pgPool, initRelatedStores) {
+		this._app = app;
+		this._pgPool = pgPool;
+		this._initRelatedStores = initRelatedStores;
+	}
+
+	init() {
+		new PgMetadataController(this._app, this._pgPool, config.pgSchema.metadata, this._initRelatedStores);
+		new PgRelationsController(this._app, this._pgPool, config.pgSchema.relations, this._initRelatedStores);
+		new PgDataSourcesController(this._app, this._pgPool, config.pgSchema.dataSources, this._initRelatedStores);
+		new PgApplicationController(this._app, this._pgPool, config.pgSchema.application, this._initRelatedStores);
+		new PgViewsController(this._app, this._pgPool, config.pgSchema.views, this._initRelatedStores);
+		new PgSpecificController(this._app, this._pgPool, config.pgSchema.specific, this._initRelatedStores);
+		new PgUserController(this._app, this._pgPool, config.pgSchema.user, this._initRelatedStores);
+		new PgDataController(this._app, this._pgPool, this._initRelatedStores);
+	}
+}
+
+module.exports = Routes;
