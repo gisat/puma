e288a5efc5c44a95337f579e68d008fb56c5e1cb
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2020-07-08 11:09:42 +0200

    Use serial for error logs

--- migrations/202006181344.do.sql
+++ migrations/202006181344.do.sql
@@ -1,8 +1,4 @@
-CREATE SEQUENCE "various"."errorLogs_key_seq" AS INT MINVALUE -2147483648 CYCLE;
-
 CREATE TABLE "various"."errorLogs"(
-    "key" INT NOT NULL PRIMARY KEY DEFAULT nextval('various."errorLogs_key_seq"'),
+    "key" SERIAL NOT NULL PRIMARY KEY,
     "data" JSONB
 );
-
-ALTER SEQUENCE "various"."errorLogs_key_seq" OWNED BY "various"."errorLogs"."key";
