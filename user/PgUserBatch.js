bebb13cb55edf287b0762b1c2968dee3b9882f3d
Martin Babič <martin.babic@gisat.cz>
2020-12-14 17:56:02 +0100

    Update notification email content

--- user/PgUserBatch.js
+++ user/PgUserBatch.js
@@ -100,7 +100,7 @@ class PgUserBatch {
 				`"GISAT LPIS update" <${this._fromEmail}>`,
 				processedUser.email,
 				`Vyhodnocení snímků z družic Sentinels pro potřeby aktualizace LPIS`,
-				`Dobrý den\nposíláme Vám Vaše přístupové údaje do aplikace pro podporu ověření žádostí o aktualizaci LPIS.\n\nhttps://lpisup.gisat.cz/\nE-Mail: ${processedUser.email}\nHeslo: ${processedUser.password}\n\nV případě jakýchkoliv technických problémů s přihlášením nebo funkčností aplikace nás informujte na e-mailové adrese lpis-admin@gisat.cz.\n\nDěkujeme.\nGISAT s.r.o.\nwww.gisat.cz\n`
+				`Dobrý den\nposíláme Vám Vaše přístupové údaje do aplikace pro detekci rizikových DPB a PB.\n\nhttps://lpischeck.gisat.cz/\nE-Mail: ${processedUser.email}\nHeslo: ${processedUser.password}\n\nV případě jakýchkoliv technických problémů s přihlášením nebo funkčností aplikace nás informujte na e-mailové adrese lpis-admin@gisat.cz.\n\nDěkujeme.\nGISAT s.r.o.\nwww.gisat.cz\n`
 			)
 			.then(() => {
 				processedUser.notified = true;
